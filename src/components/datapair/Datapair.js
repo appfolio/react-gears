7c90ec9ef1bb5f7863b504b9c4afa897f5ac974f
Aaron Panchal <aaron.panchal@appfolio.com>
2016-09-26 10:26:44 -0700

    Update datapair to use reactstrap components

--- src/components/datapair/Datapair.js
+++ src/components/datapair/Datapair.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Col, Row } from 'reactstrap';
 
 const Datapair = (props) => (
-  <div className="datapair">
-    <span>{props.label}</span>
-    <div>{props.children || props.value}</div>
-  </div>
+  <Row className="m-b-1">
+    <Col sm="4" className="text-sm-right text-muted">{props.label}</Col>
+    <Col sm="8">{props.children || props.value}</Col>
+  </Row>
 );
 
 Datapair.PropTypes = {
