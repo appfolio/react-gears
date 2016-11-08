a3963c54f938322033077822891e1ddfae14e05d
Gary Thomas <gary.thomas@appfolio.com>
2016-11-08 10:42:35 -0800

    gt - Add Select2 equivalent

---
+++ src/components/Select.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+
+// Disables CSS modules to import as global:
+import '!!style!css!postcss!sass!./Select.scss';
+
+@observer
+class Select2 extends Component {
+
+  // TODO specifying value prop makes selection impossible:
+  @observable value = this.props.value;
+  updateValue = value => this.value = value;
+
+  render() {
+    return (
+      <Select
+        onChange={this.updateValue}
+        value={this.value}
+        {...this.props}
+      />
+    );
+  }
+}
+
+export default Select2;
