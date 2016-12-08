19ac7a69cb162720d58be2adb01a56660b78e4af
Gary Thomas <gary.thomas@appfolio.com>
2016-12-08 13:38:46 -0800

    gt - Update theme picker

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,40 +9,29 @@ const ThemeLink = props => {
     link.href = url;
   }
 
-  return <Button onClick={() => changeTheme(props.url)}>{props.children}</Button>
+  return <Button color="link" onClick={() => changeTheme(props.url)}>{props.children}</Button>
 }
 
 const THEMES = [
-  { url: 'https://s3-us-west-2.amazonaws.com/appfolio-frontend-dev/styles/bootstrap.css', name: 'APM' },
-  { url: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css', name: 'Bootstrap' }
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cerulean/bootstrap.min.css', name: 'Cerulean' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css', name: 'Cosmo' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cyborg/bootstrap.min.css', name: 'Cyborg' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/darkly/bootstrap.min.css', name: 'Darkly' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css', name: 'Flatly' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/journal/bootstrap.min.css', name: 'Journal' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css', name: 'Lumen' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css', name: 'Paper' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/readable/bootstrap.min.css', name: 'Readable' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/sandstone/bootstrap.min.css', name: 'Sandstone' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/simplex/bootstrap.min.css', name: 'Simplex' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/slate/bootstrap.min.css', name: 'Slate' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/spacelab/bootstrap.min.css', name: 'Spacelab' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css', name: 'Superhero' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css', name: 'United' },
-//  { url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css', name: 'Yeti' }
+  { name: 'APM/Saffron', url: 'https://s3-us-west-2.amazonaws.com/appfolio-frontend-dev/styles/bootstrap.css' },
+  { name: 'Bootstrap', url: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css' },
+  { name: 'Admire', url: 'http://dev.lorvent.com/admire/css/components.css' },
+  { name: 'Paper', url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css' },
+  { name: 'Superhero', url: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css' },
 ];
-addDecorator((story, info) => (
+
+addDecorator((story, info) => (<div>
+  <ButtonGroup size="sm">
+    {THEMES.map(theme => <ThemeLink url={theme.url}>{theme.name}</ThemeLink>)}
+  </ButtonGroup>
   <Container fluid className="p-5">
-    <ButtonGroup>
-      {THEMES.map(theme => <ThemeLink url={theme.url}>{theme.name}</ThemeLink>)}
-    </ButtonGroup>
     <hr />
     <h1>{info.kind}</h1>
     <h3 className="text-muted">{info.story}</h3>
     <hr />
     {story()}
   </Container>
+  </div>
 ));
 
 setAddon(infoAddon);
