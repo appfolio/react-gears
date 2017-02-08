13cb9ff48b5bd9164076969d1ede1720d544e23d
Gary Thomas <gary.thomas@appfolio.com>
2017-02-08 15:45:02 -0800

    gt - Add unit tests

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -3,13 +3,17 @@ import MaskedInput from 'react-text-mask';
 import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 import { InputGroup, InputGroupAddon } from 'reactstrap';
 
+// TODO support I18n
 const CurrencyInput = ({ size, ...props }) => (
   <InputGroup size={size} className={props.className}>
     <InputGroupAddon>$</InputGroupAddon>
     <MaskedInput
       {...props}
       className="form-control"
-      mask={createNumberMask({ allowDecimal: true, prefix: '' })}
+      mask={createNumberMask({
+        allowDecimal: true,
+        prefix: ''
+      })}
     />
   </InputGroup>
 );
---
+++ test/components/CurrencyInput.js
@@ -0,0 +1,69 @@
+/* eslint-env mocha */
+import React from 'react';
+import assert from 'assert';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import { CurrencyInput } from '../../src';
+
+describe.only('<CurrencyInput />', () => {
+  const callback = sinon.spy();
+
+//  it('should have address1', () => {
+//    const input = component.find('[name="address1"]');
+//    assert.equal(input.prop('placeholder'), 'Address 1');
+//    assert.equal(input.prop('defaultValue'), 'Wayne Enterprises');
+//    assert.equal(input.prop('value'), undefined);
+//
+//    input.simulate('change', { target: { name: 'address1', value: 'Batcave' }});
+//    assert(callback.calledWith({ address1: 'Batcave' }));
+//  });
+
+  it('should default to blank', () => {
+    const component = mount(<CurrencyInput onChange={callback} />);
+    const input = component.find('input');
+
+    assert.equal(input.get(0).value, '');
+  });
+
+  it('should format value prop', () => {
+    const component = mount(<CurrencyInput onChange={callback} value={1234.56} />);
+    const input = component.find('input');
+
+    assert.equal(input.get(0).value, '1,234.56');
+  });
+
+  it('should accept comma formatted values', () => {
+    const component = mount(<CurrencyInput onChange={callback} value="123,456,789.99999" />);
+    const input = component.find('input');
+    assert.equal(input.get(0).value, '123,456,789.99');
+  });
+
+  it('should not format invalid values', () => {
+    const component = mount(<CurrencyInput onChange={callback} value="Veni, Vedi, Vici" />);
+    const input = component.find('input');
+    assert.equal(input.get(0).value, '');
+  });
+
+  it('should truncate more than 2 decimals', () => {
+    const component = mount(<CurrencyInput onChange={callback} value={1234.56789} />);
+    const input = component.find('input');
+
+    assert.equal(input.get(0).value, '1,234.56');
+  });
+
+  // TODO skipped pending this issue/question: https://github.com/text-mask/text-mask/issues/372
+  it.skip('should zero pad 1 decimal', () => {
+    const component = mount(<CurrencyInput onChange={callback} value={1234.5} />);
+    const input = component.find('input');
+
+    assert.equal(input.get(0).value, '1,234.50');
+  });
+
+  it.skip('should zero pad no decimals', () => {
+    const component = mount(<CurrencyInput onChange={callback} value={1234} />);
+    const input = component.find('input');
+
+    assert.equal(input.get(0).value, '1,234.00');
+  });
+});
