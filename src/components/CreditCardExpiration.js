e85fa51a0dadb9dd2e6f4c1eb8e066c71c7a4025
Troy Alford <troy@troyalford.com>
2017-04-04 12:03:20 -0700

    Refactored to use Select instead of ButtonGroups

--- src/components/CreditCardExpiration.js
+++ src/components/CreditCardExpiration.js
@@ -1,11 +1,9 @@
 import React, { Component, PropTypes } from 'react';
-import {
-  ButtonDropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  InputGroup,
-} from 'reactstrap';
+import autoBind from 'react-autobind';
+import { Select } from '../';
+import { InputGroup } from 'reactstrap';
+
+import STYLES from './CreditCardExpiration.scss';
 
 const today = new Date();
 const MONTHS = [
@@ -15,62 +13,34 @@ const MONTHS = [
 const YEARS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   .map(offset => today.getFullYear() + offset);
 
+// eslint-disable-next-line arrow-body-style
+const monthOptions = MONTHS.map((label, index) => ({ label, value: index + 1 }));
+// eslint-disable-next-line arrow-body-style
+const yearsOptions = YEARS.map(year => ({ label: year, value: year }));
+
 export default class CreditCardExpiration extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showMonthPicker: false, showYearPicker: false };
+  constructor(props) { super(props); autoBind(this); }
 
-    this.closePickers = this.closePickers.bind(this);
-    this.toggleMonthPicker = this.toggleMonthPicker.bind(this);
-    this.toggleYearPicker = this.toggleYearPicker.bind(this);
-    this.onMonthSelection = this.onMonthSelection.bind(this);
-    this.onYearSelection = this.onYearSelection.bind(this);
-  }
-
-  onMonthSelection(month) {
-    this.closePickers();
+  onMonthSelection(option) {
+    const month = option && option.value || today.getMonth();
     this.props.onChange({ month, year: this.props.year });
   }
-  onYearSelection(year) {
-    this.closePickers();
+  onYearSelection(option) {
+    const year = option && option.value || today.getFullYear();
     this.props.onChange({ year, month: this.props.month });
-  }
-
-  closePickers() {
-    this.setState({ showMonthPicker: false, showYearPicker: false });
-  }
-  toggleMonthPicker() {
-    this.setState({
-      showMonthPicker: !this.state.showMonthPicker,
-      showYearPicker: false,
-    });
-  }
-  toggleYearPicker() {
-    this.setState({
-      showYearPicker: !this.state.showYearPicker,
-      showMonthPicker: false,
-    });
   }
 
   render() {
     return (
       <InputGroup>
-        <ButtonDropdown isOpen={this.state.showMonthPicker} toggle={this.toggleMonthPicker}>
-          <DropdownToggle caret>{MONTHS[this.props.month]}</DropdownToggle>
-          <DropdownMenu>
-            {MONTHS.map((name, number) =>
-              <DropdownItem onClick={() => this.onMonthSelection(number + 1)}>{name}</DropdownItem>
-            )}
-          </DropdownMenu>
-        </ButtonDropdown>
-        <ButtonDropdown isOpen={this.state.showYearPicker} toggle={this.toggleYearPicker}>
-          <DropdownToggle caret>{this.props.year}</DropdownToggle>
-          <DropdownMenu>
-            {YEARS.map(year =>
-              <DropdownItem onClick={() => this.onYearSelection(year)}>{year}</DropdownItem>
-            )}
-          </DropdownMenu>
-        </ButtonDropdown>
+        <Select
+          className={STYLES.select} placeholder="Month" defaultValue={this.props.month}
+          options={monthOptions} onChange={this.onMonthSelection}
+        />
+        <Select
+          className={STYLES.select} placeholder="Year" defaultValue={this.props.year}
+          options={yearsOptions} onChange={this.onYearSelection}
+        />
       </InputGroup>
     );
   }
