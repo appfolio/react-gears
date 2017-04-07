99332c9a2ba2dfaff3dd255a67341969b13e7614
Troy Alford <troy@troyalford.com>
2017-04-06 17:21:51 -0700

    Adding onChange pattern enforcement, and isValid reporting to PatternInput

--- src/components/PatternInput.js
+++ src/components/PatternInput.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Input } from 'reactstrap';
+import autoBind from 'react-autobind';
 
-function restrictKeystrokeTo(e, pattern) {
-  if (!pattern.exec(String.fromCharCode(e.charCode))) {
-    e.preventDefault();
+export default class PatternInput extends Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+
+    if (!!props.pattern.exec(props.value || '')) {
+      this.lastValidValue = props.value;
+    } else {
+      this.lastValidValue = '';
+    }
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    const { pattern, restrictInput } = this.props;
+    const isValid = !!pattern.exec(value);
+
+    if (!isValid && restrictInput) {
+      this.input.value = this.lastValidValue || '';
+    } else {
+      if (isValid) this.lastValidValue = value;
+
+      this.props.onChange({ value, isValid });
+    }
+  }
+  handleKeyPress(event) {
+    if (!this.props.restrictInput) return;
+    if (!this.props.pattern.exec(String.fromCharCode(event.charCode))) {
+      event.preventDefault();
+    }
+  }
+
+  render() {
+    const { pattern, restrictInput, ...inputProps } = this.props;
+    return (
+      <Input
+        {...inputProps} ref={self => { this.input = self; }}
+        onKeyPress={!!restrictInput && this.handleKeyPress}
+        onChange={this.handleChange} pattern={pattern}
+      />
+    );
   }
 }
 
-const PatternInput = ({ pattern, ...props }) => (
-  <Input {...props} onKeyPress={pattern ? e => restrictKeystrokeTo(e, pattern) : null} />
-);
+PatternInput.defaultProps = {
+  pattern: /.*/g,
+  restrictInput: true,
+  value: '',
 
-PatternInput.propTypes = {
-  pattern: React.PropTypes.object
+  onChange: () => {},
 };
+PatternInput.propTypes = {
+  pattern: (props, propName) => (
+    props[propName] instanceof RegExp ? null : new Error('Pattern must be a valid RegExp')
+  ),
+  restrictInput: PropTypes.bool,
+  value: PropTypes.string,
 
-export default PatternInput;
+  onChange: PropTypes.func,
+};
--- test/components/PatternInput.spec.js
+++ test/components/PatternInput.spec.js
@@ -2,24 +2,47 @@
 import React from 'react';
 import assert from 'assert';
 import sinon from 'sinon';
-import { shallow } from 'enzyme';
-import { Input } from 'reactstrap';
+import { mount, shallow } from 'enzyme';
 
 import PatternInput from '../../src/components/PatternInput';
 
 describe('<PatternInput />', () => {
-  it('should wrap a plain Input', () => {
-    const input = shallow(<PatternInput coolProp="value" />);
-    assert.equal(input.is(Input), true);
-    assert.equal(input.prop('onKeyPress'), null);
-  });
-
   it('should block non allowed characters', () => {
     const input = shallow(<PatternInput pattern={/[a7]/} />);
+    const preventDefault = sinon.spy();
     assert(input.prop('onKeyPress'));
-    let preventDefault = sinon.spy();
-    input.simulate('keyPress', { charCode: 97, preventDefault });
-    input.simulate('keyPress', { charCode: 98, preventDefault });
+
+    input.simulate('keyPress', { charCode: 'a'.charCodeAt(0), preventDefault });
+    assert(!preventDefault.called);
+
+    input.simulate('keyPress', { charCode: 'b'.charCodeAt(0), preventDefault });
     assert(preventDefault.calledOnce);
+  });
+
+  it('should not block input if restrictInput is false', () => {
+    const preventDefault = sinon.spy();
+    const input = shallow(<PatternInput restrictInput={false} pattern={/^[0-9]*$/} />);
+    input.simulate('keyPress', { charCode: 'a'.charCodeAt(0), preventDefault });
+    assert(!preventDefault.called);
+  });
+
+  it('restrictInput should only prevent onChange for invalid input', () => {
+    const onChange = sinon.spy();
+    const component = mount(
+      <PatternInput restrictInput pattern={/^[0-9]*$/} onChange={onChange} />
+    );
+    const input = component.find('input').get(0);
+
+    assert.equal(input.value, '');
+
+    // Valid input
+    component.simulate('change', { target: { value: '123' } });
+    assert(onChange.called);  // << onChange event
+    assert.equal(input.value, '');
+    onChange.reset();
+
+    // Invalid input
+    component.simulate('change', { target: { value: 'abc' } });
+    assert(!onChange.called); // << No onChange event
   });
 });
