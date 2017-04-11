11693c8139c67b7e41b335a8cf3c3b54102ce713
Gary Thomas <gary.thomas@appfolio.com>
2017-04-10 23:17:53 -0700

    gt - Remove editable prop
    Use presence of onEdit, onDelete, onUndelete to show/hide actions

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Alert, Button, Card, CardBlock, CardHeader, CardText, Flag, Input } from '../';
 
 import fecha from 'fecha';
-import styles from './Note.scss';
 
 // TODO extract to date helper, i18n:
 const format = date => fecha.format(date, 'ddd, MMMM d, YYYY "at" h:mm A');
 
 const DeletedNote = ({ note, onUndelete }) => (
   <Alert color="success" icon>
-    Note deleted. <a href="#" onClick={() => onUndelete(note)}>undo</a>
+    Note deleted.
+    {onUndelete ? <a href="#" className="ml-1" onClick={() => onUndelete(note)}>undo</a> : null}
   </Alert>
 );
 
-const EditableNote = ({ note, onEdit }) => (
+const EditableText = ({ note, onEdit }) => (
   <Card>
     <CardBlock>
       <Input type="textarea" value={note.text} rows="5" />
@@ -30,38 +30,38 @@ const EditableNote = ({ note, onEdit }) => (
 );
 
 const Note = note => {
-  const { children, from, date, editable, edited, editing, deleted, text, onDelete, onEdit, onUndelete } = note;
+  const { children, className, date, deleted, edited, editing, from, text, onDelete, onEdit, onUndelete } = note;
   return (
-    <div className="mb-3">
-      {deleted ? <DeletedNote note={note} onUndelete={onUndelete} /> : editing ?
-        <EditableNote note={note} onEdit={onEdit} /> : (
-        <Card className="rounded-top">
-          <CardHeader className="d-flex justify-content-start p-2 rounded-top bg-faded">
-            {edited ? <span><Flag color="primary text-uppercase mr-2">Edited</Flag></span> : null}
-            <span className="text-muted">
-              {edited ? 'Last edited' : 'Posted'} {from ? `by ${from} ` : ' '}on {format(date)}
-            </span>
-            {editable ? (
-              <span className="ml-auto">
-                <a href="#" onClick={() => onEdit(note)} className="mr-3">edit</a>
-                <a href="#" onClick={() => onDelete(note)}>delete</a>
+    <div className={`mb-3 ${className}`}>
+      {deleted ?
+        <DeletedNote note={note} onUndelete={onUndelete} /> :
+        editing ?
+          <EditableText note={note} onEdit={onEdit} /> :
+          <Card className="rounded-top">
+            <CardHeader className="d-flex justify-content-start p-2 rounded-top bg-faded">
+              {edited ? <span><Flag color="primary text-uppercase mr-2">Edited</Flag></span> : null}
+              <span className="text-muted">
+                {edited ? 'Last edited' : 'Posted'} {from ? `by ${from} ` : ' '}on {format(date)}
               </span>
-            ) : null}
-          </CardHeader>
-          <CardBlock>
-            <CardText className={styles.text}>{text}</CardText>
-            {children}
-          </CardBlock>
-        </Card>
-      )}
+              <span className="ml-auto">
+                {onEdit ? <a href="#" onClick={() => onEdit(note)} className="mr-3">edit</a> : null}
+                {onDelete ? <a href="#" onClick={() => onDelete(note)}>delete</a> : null}
+              </span>
+            </CardHeader>
+            <CardBlock>
+              <CardText style={{ whiteSpace: 'pre-wrap' }}>{text}</CardText>
+              {children}
+            </CardBlock>
+          </Card>
+       }
     </div>
   );
 };
 
 Note.propTypes = {
+  className: React.PropTypes.string,
   date: React.PropTypes.object,
   deleted: React.PropTypes.bool,
-  editable: React.PropTypes.bool,
   edited: React.PropTypes.bool,
   editing: React.PropTypes.bool,
   from: React.PropTypes.string,
