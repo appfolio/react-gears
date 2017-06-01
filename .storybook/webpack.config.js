92cd9325ec2ea490c8cc41931ac18d913b6624b5
Gary Thomas <gary.thomas@appfolio.com>
2017-06-01 12:03:15 -0700

    gt - Update storybook config to webpack 2

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        loader: 'style-loader!css-loader?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader'
+      }
+    ]
+  },
   plugins: [
     new webpack.LoaderOptionsPlugin({
       options: {
@@ -11,14 +19,5 @@ module.exports = {
         ]
       }
     })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.s?css$/,
-        loader: 'style-loader!css-loader?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader',
-        include: path.resolve(__dirname, '../')
-      }
-    ],
-  },
+  ]
 };
