d63d73eb783d7e485e496053781a9c569688519c
Aaron Panchal <aaron.panchal@appfolio.com>
2017-06-05 10:06:57 -0700

    ap - improve HasManyField components' spacing

--- src/components/HasManyFieldsAdd.js
+++ src/components/HasManyFieldsAdd.js
@@ -3,8 +3,8 @@ import { Button } from 'reactstrap';
 import Icon from './Icon';
 
 const HasManyFieldsAdd = ({ children, outline, color, ...props }) => (
-  <Button outline color="success" {...props} className="w-100">
-    <Icon name="plus" className="mr-2" />
+  <Button outline color="success" {...props} className="w-100 rounded-0">
+    <Icon name="plus-circle" className="mr-2" />
     <span children={children} />
   </Button>
 );
--- src/components/HasManyFieldsRow.js
+++ src/components/HasManyFieldsRow.js
@@ -4,11 +4,11 @@ import { Row, Col, Button } from 'reactstrap';
 import Icon from './Icon';
 
 const HasManyFieldsRow = ({ children, onDelete }) => (
-  <Row className="mb-3">
+  <Row className="mb-3" noGutters>
     <Col children={children} />
-    <Col xs="auto" className="pr-3">
-      <Button outline color="danger" onClick={onDelete} className="h-100">
-        <Icon name="remove" />
+    <Col xs="auto" className="pl-3">
+      <Button outline color="danger" onClick={onDelete} className="h-100 rounded-0 p-2">
+        <Icon name="trash-o" size="lg"/>
       </Button>
     </Col>
   </Row>
