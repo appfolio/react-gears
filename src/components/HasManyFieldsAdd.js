336fe2dbe2f88b578cb379811bc426456ad884c5
Aaron Panchal <aaron.panchal@appfolio.com>
2017-06-05 10:30:14 -0700

    ap - add tests for HasManyFieldsAdd component

--- src/components/HasManyFieldsAdd.js
+++ src/components/HasManyFieldsAdd.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import classNames from 'classnames';
 import Icon from './Icon';
 
-const HasManyFieldsAdd = ({ children, outline, color, ...props }) => (
-  <Button outline color="success" {...props} className="w-100 rounded-0">
-    <Icon name="plus-circle" className="mr-2" />
-    <span children={children} />
-  </Button>
-);
+const HasManyFieldsAdd = ({ children, outline, color, className, ...props }) => {
+  const classes = classNames('w-100 rounded-0', className);
+
+  return (
+    <Button outline color="success" {...props} className={classes}>
+      <Icon name="plus-circle" className="mr-2" />
+      <span children={children} />
+    </Button>
+  );
+}
+
+HasManyFieldsAdd.propTypes = {
+  children: React.PropTypes.node.isRequired
+};
 
 export default HasManyFieldsAdd;
