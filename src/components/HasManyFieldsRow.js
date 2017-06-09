9efc30e873216ee27bac96a9ab4989278b42fa53
Aaron Panchal <aaron.panchal@appfolio.com>
2017-06-09 10:40:59 -0700

    ap - fix height of HasManyFieldsRemove delete button on Safari

--- src/components/HasManyFieldsRow.js
+++ src/components/HasManyFieldsRow.js
@@ -6,8 +6,8 @@ import Icon from './Icon';
 const HasManyFieldsRow = ({ children, onDelete }) => (
   <Row className="mb-3" noGutters>
     <Col children={children} />
-    <Col xs="auto" className="pl-3">
-      <Button outline color="danger" onClick={onDelete} className="h-100 rounded-0 p-2">
+    <Col xs="auto" className="pl-3 d-flex">
+      <Button outline color="danger" onClick={onDelete} className="rounded-0 p-2">
         <Icon name="trash-o" size="lg" />
       </Button>
     </Col>
