96bcccfe411d543485583d3420b5f662531f6e42
Gary Thomas <gary.thomas@appfolio.com>
2017-06-26 19:25:13 -0700

    gt/ps - Initial controlled DateInput work

--- stories/DateInput.js
+++ stories/DateInput.js
@@ -14,11 +14,28 @@ storiesOf('DateInput', module)
   ))
   .addWithInfo('defaultValue', () => (
     <div>
-      <FormRow type={DateInput} label="null" />
-      <FormRow type={DateInput} label="new Date()" defaultValue={new Date()} />
-      <FormRow type={DateInput} label="new Date(2000, 0, 1)" defaultValue={new Date(2000, 0, 1)} />
-      <FormRow type={DateInput} label="'1/23/2004'" defaultValue="1/23/2004" />
-      <FormRow type={DateInput} label="'Garbage in'" defaultValue="Garbage in" />
+      <p>
+        When defaultValue is set, component is 'uncontrolled' and maintains its own state.
+        onChange events will be emitted with the current value.
+      </p>
+      <FormRow type={DateInput} onChange={action('onChange')} label="null" />
+      <FormRow type={DateInput} onChange={action('onChange')} label="new Date()" defaultValue={new Date()} />
+      <FormRow type={DateInput} onChange={action('onChange')} label="new Date(2000, 0, 1)" defaultValue={new Date(2000, 0, 1)} />
+      <FormRow type={DateInput} onChange={action('onChange')} label="'1/23/2004'" defaultValue="1/23/2004" />
+      <FormRow type={DateInput} onChange={action('onChange')} label="'Garbage in'" defaultValue="Garbage in" />
+    </div>
+  ))
+  .addWithInfo('value', () => (
+    <div>
+      <p>
+        When value is set, component is 'controlled' and does not maintain its own state.
+        onChange events will be emitted with the current value, and parent components using the DateInput must update the value prop with the current date.
+      </p>
+      <FormRow type={DateInput} onChange={action('onChange')} label="null" />
+      <FormRow type={DateInput} onChange={action('onChange')} label="new Date()" value={new Date()} />
+      <FormRow type={DateInput} onChange={action('onChange')} label="new Date(2000, 0, 1)" value={new Date(2000, 0, 1)} />
+      <FormRow type={DateInput} onChange={action('onChange')} label="'1/23/2004'" value="1/23/2004" />
+      <FormRow type={DateInput} onChange={action('onChange')} label="'Garbage in'" value="Garbage in" />
     </div>
   ))
   .addWithInfo('Calendar', () => (
