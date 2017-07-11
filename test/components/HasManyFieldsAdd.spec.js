e6eb1345a619767cf8d25140ad437d849971b3e5
Michael Jewell <michaeljewell9911@gmail.com>
2017-07-11 15:41:45 -0700

    mj - add tests for has many fields

--- test/components/HasManyFieldsAdd.spec.js
+++ test/components/HasManyFieldsAdd.spec.js
@@ -1,13 +1,19 @@
-import React from 'react';
 import assert from 'assert';
-import { shallow } from 'enzyme';
 
+import { shallow } from 'enzyme';
+import React from 'react';
 import { Button } from 'reactstrap';
-import { Icon, HasManyFieldsAdd } from '../../src';
+
+import { HasManyFieldsAdd, Icon } from '../../src';
 
 describe('<HasManyFieldsAdd />', () => {
   const component = shallow(
-    <HasManyFieldsAdd outline={false} color="danger" className="foo" random="prop">
+    <HasManyFieldsAdd
+      outline={false}
+      color="danger"
+      className="foo"
+      random="prop"
+    >
       Custom Label!
     </HasManyFieldsAdd>
   );
@@ -22,19 +28,32 @@ describe('<HasManyFieldsAdd />', () => {
   });
 
   it('should forward props', () =>
-    assert.equal(component.prop('random'), 'prop')
-  );
+    assert.equal(component.prop('random'), 'prop'));
 
-  it('should be a button', () =>
-    assert.equal(component.type(), Button)
-  );
+  it('should be a button', () => assert.equal(component.type(), Button));
 
   it('should have customizable text', () =>
-    assert.equal(component.find('span').text(), 'Custom Label!')
-  );
+    assert.equal(component.find('span').text(), 'Custom Label!'));
 
   it('should have an icon', () => {
     const icon = component.find(Icon);
     assert.equal(icon.prop('name'), 'plus-circle');
+  });
+
+  it('should be a disabled button when disabled', () => {
+    const disabledComponent = shallow(
+      <HasManyFieldsAdd
+        outline={false}
+        color="danger"
+        className="foo"
+        random="prop"
+        disabled
+      >
+        Custom Label!
+      </HasManyFieldsAdd>
+    );
+
+    assert.equal(disabledComponent.prop('disabled'), true);
+    assert.equal(disabledComponent.prop('color'), 'secondary');
   });
 });
