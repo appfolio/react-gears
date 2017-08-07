96b2dd6aabc70a776410a60b13059dba7c33b04c
Paulus Schoutsen <paulus.schoutsen@mycase.com>
2017-08-07 13:12:52 -0700

    SCSS compile CSS output

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import css from 'modular-css-rollup';
 import scssSyntax from 'postcss-scss';
-
+import sass from 'node-sass';
+import packageImporter from 'node-sass-package-importer';
 
 // `require` understands JSON
 const packageJSON = require('./package.json');
@@ -23,17 +24,45 @@ function isExternal(dependency) {
   return false;
 }
 
+/**
+ * Routes the output of modular CSS through SASS
+ * before writing it to a file.
+ */
+function patchedModularCSS() {
+  const plugin = css({
+    ext: '.scss',
+    parser: scssSyntax,
+    css: 'dist/reactgears.css',
+    exportGlobals: false,
+  });
+
+  const oldOnWrite = plugin.onwrite;
+  plugin.onwrite = (bundle, result) => {
+    const patchedResult = Object.assign({}, plugin, {
+      css: result.css.then(data => {
+        return {
+          css: sass.renderSync({
+            data: data.css,
+            // To support Sass imports using ~
+            importer: packageImporter()
+          }).css.toString()
+        };
+      })
+    });
+
+    return oldOnWrite(bundle, patchedResult);
+  };
+
+  return plugin;
+}
+
 const config = {
   sourceMap: true,
   moduleName: 'ReactGears',
   entry: 'src/index.js',
   exports: 'named',
   plugins: [
-    css({
-      ext: '.scss',
-      parser: scssSyntax,
-      css: 'dist/reactgears.css',
-    }),
+    patchedModularCSS(),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
