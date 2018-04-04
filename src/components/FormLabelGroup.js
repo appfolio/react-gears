61be1f2c680b716c0095cb7e94f1d01538b8bedd
Gary Thomas <gary.thomas@appfolio.com>
2018-04-04 15:10:09 -0700

    gt - Add props to match FormRow
    WIP

--- src/components/FormLabelGroup.js
+++ src/components/FormLabelGroup.js
@@ -5,7 +5,7 @@ import FormGroup from './FormGroup';
 import Label from './Label';
 import Row from './Row';
 
-const FormLabelGroup = props => {
+const FormLabelGroup = (props) => {
   const {
     children,
     color,
@@ -14,7 +14,9 @@ const FormLabelGroup = props => {
     label,
     required,
     rowClassName,
+    size,
     stacked,
+    width
   } = props;
 
   const rowColor = color || (invalid && 'danger');
@@ -25,14 +27,16 @@ const FormLabelGroup = props => {
   return (
     <FormGroup row className={rowClassName} color={rowColor}>
       {label && (
-        <Label for={inputId} sm={labelWidth} className={labelAlignment}>
+        <Label for={inputId} sm={labelWidth} size={size} className={labelAlignment}>
           {label}
           {required && label ? <span className="text-danger">&nbsp;*</span> : null}
         </Label>
       )}
       <Col sm={inputContainerWidth}>
         <Row>
-          {children}
+          <Col {...width}>
+            {children}
+          </Col>
         </Row>
       </Col>
     </FormGroup>
@@ -42,16 +46,28 @@ const FormLabelGroup = props => {
 FormLabelGroup.propTypes = {
   children: PropTypes.node.isRequired,
   color: PropTypes.string,
+  feedback: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]),
+  inline: PropTypes.bool,
   inputId: PropTypes.string,
   invalid: PropTypes.bool,
+  hint: PropTypes.string,
   label: PropTypes.node,
   required: PropTypes.bool,
   rowClassName: PropTypes.string,
+  size: PropTypes.string,
   stacked: PropTypes.bool,
+  width: PropTypes.object
 };
 
 FormLabelGroup.defaultProps = {
+  inline: false,
   invalid: false,
+  required: false,
+  stacked: false,
+  width: { xs: 12 }
 };
 
 export default FormLabelGroup;
