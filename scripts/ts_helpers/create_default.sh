cf95c655650838ca19054f75f3ebef8bc5180c91
Steve Klebanoff <steve.klebanoff@mycase.com>
2018-04-24 09:49:35 -0700

    sk: remove scripts which were mostly just for myself

--- scripts/ts_helpers/create_default.sh
+++
@@ -1,5 +0,0 @@
-
-cat >"$1.d.ts" <<EOF
-import $1 from 'reactstrap/lib/$1';
-export default $1;
-EOF
--- scripts/ts_helpers/create_empty.sh
+++
@@ -1,8 +0,0 @@
-
-cat >"./src/components/$1.d.ts" <<EOF
-export interface $1PropTypes {
-
-}
-declare const $1: React.StatelessComponent<$1PropTypes>;
-export default $1;
-EOF
