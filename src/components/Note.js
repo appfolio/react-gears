3fac5cff95de482014dc40edcb51dc3d16b40d27
Gary Thomas <gary.thomas@appfolio.com>
2018-05-04 12:33:27 -0700

    gt - Update Note component
    - Use info BlockPanel

--- src/components/Note.js
+++ src/components/Note.js
@@ -26,7 +26,7 @@ class Note extends React.Component {
     onSave: PropTypes.func,
     onUndelete: PropTypes.func,
     rows: PropTypes.number,
-    saving: PropTypes.bool
+    saving: PropTypes.boolean
   };
 
   static defaultProps = {
