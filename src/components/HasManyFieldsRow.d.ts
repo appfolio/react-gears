be63c4a5e1f572705fca509ce5ad92270fb3d4d1
Steve Klebanoff <steve.klebanoff@mycase.com>
2018-05-18 14:55:22 -0700

    sk: update for new disabledReason prop

--- src/components/HasManyFieldsRow.d.ts
+++ src/components/HasManyFieldsRow.d.ts
@@ -4,6 +4,7 @@ interface HasManyFieldsRowProps {
   children: (JSX.Element | string) | (JSX.Element | string)[];
   onDelete?: React.MouseEventHandler<any>;
   disabled?: boolean;
+  disabledReason: (JSX.Element | string) | (JSX.Element | string)[];
 }
 declare class HasManyFieldsRow extends React.Component<HasManyFieldsRowProps, {}> { }
 export default HasManyFieldsRow;
