267bda3412dbb1b2df62b2938afc70275b2a76bd
Gary Thomas <gary.thomas@appfolio.com>
2018-10-11 09:13:51 -0700

    gt - Correct CheckBoxBooleanInput
    - Add className support
    - Remove string ref

--- src/components/CheckboxBooleanInput.js
+++ src/components/CheckboxBooleanInput.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import classnames from 'classnames';
 import FormGroup from './FormGroup';
 import Input from './Input';
 import Label from './Label';
@@ -7,15 +8,17 @@ import Label from './Label';
 class CheckboxBooleanInput extends React.Component {
   static propTypes = {
     checkboxLabel: PropTypes.node,
+    className: PropTypes.string,
     onChange: PropTypes.func,
     value: PropTypes.bool
   };
 
   render() {
-    const { checkboxLabel, onChange, value, id, ...inputProps } = this.props;
+    const { checkboxLabel, className, onChange, value, id, ...inputProps } = this.props;
+    const classNames = classnames('col-form-label d-flex align-items-center h-100', className);
 
     return (
-      <FormGroup check className="col-form-label d-flex align-items-center h-100">
+      <FormGroup check className={classNames}>
         <Input
           id={id}
           {...inputProps}
