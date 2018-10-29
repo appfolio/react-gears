46b1a5e8a4b6d84c7b7923ac9bd371b26cb630fb
Darren Eng <darren.eng@appfolio.com>
2018-10-29 16:52:51 -0700

    de - update Close to accept className without overriding existing

--- src/components/Close.js
+++ src/components/Close.js
@@ -1,9 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const Close = props => (
-  <button type="button" className="close" aria-label="Close" {...props}>
+const Close = ({ className, ...props }) => (
+  <button type="button" className={`close ${className}`} aria-label="Close" {...props}>
     <span aria-hidden="true">&times;</span>
   </button>
 );
+
+Close.propTypes = {
+  className: PropTypes.string
+};
+
+Close.defaultProps = {
+  className: ''
+};
 
 export default Close;
