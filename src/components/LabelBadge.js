fd52a3d3850f63332e856b9bf6f40584d7680688
Gary Thomas <gary.thomas@appfolio.com>
2018-10-29 17:26:42 -0700

    gt - Update LabelBadge with bootstrap utils
    Remove custom CSS

--- src/components/LabelBadge.js
+++ src/components/LabelBadge.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Close from './Close';
-import styles from './LabelBadge.scss';
 
 export default class LabelBadge extends React.Component {
   static propTypes = {
@@ -22,7 +21,7 @@ export default class LabelBadge extends React.Component {
   render() {
     const { className, label, maxWidth, onRemove, removable, value } = this.props;
     const labelClasses = 'bg-light text-muted rounded-left d-inline-flex align-self-center h-100 px-3 py-2';
-    const valueClasses = `label-badge-value ${styles.trim} rounded-right px-3 py-2`;
+    const valueClasses = 'label-badge-value text-truncate rounded-right px-3 py-2';
     const style = {
       maxWidth: maxWidth ? `${maxWidth}rem` : null
     };
@@ -30,10 +29,10 @@ export default class LabelBadge extends React.Component {
     return (
       //TODO: use '.card' instead of hard coded boarder style for label badge
       // we aren't doing it right now because apm and bootstrap-apm has a conflict for .card
-      <span className={`rounded d-inline-flex flex-row justify-content-between align-items-center ${className} ${styles.outer}`}>
+      <span className={`border rounded d-inline-flex flex-row justify-content-between align-items-center ${className}`}>
         {label ?
           <strong className={labelClasses} style={style}>
-            <span className={styles.trim}>{label}</span>
+            <span className="text-truncate">{label}</span>
           </strong>
           : null}
         <span className={valueClasses} style={style}>{value}</span>
