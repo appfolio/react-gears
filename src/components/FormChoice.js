d3888e081b5b49186ac9ce01deb879dec4646017
Gary Thomas <gary.thomas@appfolio.com>
2018-11-07 18:49:30 -0800

    gt - Correct FormChoice
    - Remove unused selected & checked props
    - Make label clickable

--- src/components/FormChoice.js
+++ src/components/FormChoice.js
@@ -5,25 +5,35 @@ import FormGroup from './FormGroup';
 import Input from './Input';
 import Label from './Label';
 
+let count = 0;
+
+function getID() {
+  return `form-choice-${count++}`; // eslint-disable-line no-plusplus
+}
+
 class FormChoice extends React.Component {
   static propTypes = {
     inline: PropTypes.bool,
     disabled: PropTypes.bool,
-    checked: PropTypes.bool,
     children: PropTypes.node,
+    id: PropTypes.string,
     type: PropTypes.oneOf(['checkbox', 'radio', 'select']),
-    value: PropTypes.any,
-    selected: PropTypes.bool
+    value: PropTypes.any
   }
+
+  constructor(props) {
+    super(props);
+    this.id = props.id || getID();
+  }
+
   render() {
     const {
+      id,
       inline,
       disabled,
-      checked,
       children,
       type,
       value,
-      selected,
       ...attributes
     } = this.props;
 
@@ -35,28 +45,23 @@ class FormChoice extends React.Component {
 
     const computedValue = value || children;
 
-    let computedChecked;
-
-    if (type === 'checkbox') {
-      computedChecked = selected && selected.indexOf(computedValue) > -1;
-    } else if (type === 'radio') {
-      computedChecked = selected && selected === computedValue;
-    } else {
+    if (type !== 'checkbox' && type !== 'radio') {
       throw new Error(`Type '${type}' is not supported`);
     }
 
     const item = (
       <div className={labelClasses} check={!inline}>
         <Input
+          id={this.id}
           type={type}
           {...attributes}
           disabled={disabled}
           value={computedValue}
-          checked={computedChecked}
           style={{ cursor: disabled && 'not-allowed' }}
         />
         <Label
           className="form-check-label"
+          for={this.id}
           style={{ cursor: disabled && 'not-allowed' }}
         >
           {children}
