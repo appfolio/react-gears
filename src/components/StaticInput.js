ce2baf262c68b9a59009a9f10d1a17f06f0acfcc
Gary Thomas <gary.thomas@appfolio.com>
2019-01-05 12:00:01 -0800

    gt - Remove unused props from StaticInput

--- src/components/StaticInput.js
+++ src/components/StaticInput.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Input from './Input';
 
-const StaticInput = ({ type, children, color, state, ...props }) => (
+const StaticInput = ({ type, children, ...props }) => (
   <Input
     plaintext
     {...props}
@@ -12,9 +12,7 @@ const StaticInput = ({ type, children, color, state, ...props }) => (
 
 StaticInput.propTypes = {
   type: PropTypes.any,
-  children: PropTypes.any,
-  color: PropTypes.string,
-  state: PropTypes.string
+  children: PropTypes.any
 };
 
 export default StaticInput;
