acfdd877ae6d81194db731c908c8ca9e4e689f19
Gary Thomas <gary.thomas@appfolio.com>
2019-01-15 11:41:26 -0800

    gt - Update Popover default to keep previous behavior
    Ensure click outside will close

--- src/components/Popover.js
+++ src/components/Popover.js
@@ -3,7 +3,13 @@ import Popover from 'reactstrap/lib/Popover';
 
 Popover.propTypes = {
   ...Popover.propTypes,
-  placement: PropTypes.oneOf(['top', 'bottom', 'left', 'right']) // Overrides reactstrap's unsupported options.
+  placement: PropTypes.oneOf(['top', 'bottom', 'left', 'right']), // Overrides reactstrap's unsupported options.
+  trigger: PropTypes.string
+};
+
+Popover.defaultProps = {
+  ...Popover.defaultProps,
+  trigger: 'focus'
 };
 
 export default Popover;
