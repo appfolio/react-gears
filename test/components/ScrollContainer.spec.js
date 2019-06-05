77bafb9cd418d674e8819fb5b237a481398afd0b
Gary Thomas <gary.thomas@appfolio.com>
2019-06-05 13:56:32 -0700

    gt - Add tests, simplify stories

---
+++ test/components/ScrollContainer.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import assert from 'assert';
+import { mount } from 'enzyme';
+
+import { ScrollContainer } from '../../src';
+
+describe('<ScrollContainer />', () => {
+  it('should render correctly', () => {
+    const component = mount(<ScrollContainer><div style={{ minHeight: '200px' }} /></ScrollContainer>);
+    assert(component);
+  });
+
+  it('should not overflow when children less than width and no height specified');
+  it('should overflow horizontally');
+  it('should overflow vertically when height is specified');
+});
