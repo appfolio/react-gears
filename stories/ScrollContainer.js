8d6b8c9d04d26dd14bce916b717e338da6c04c14
Gary Thomas <gary.thomas@github.com>
2019-07-08 11:18:14 -0700

    gt - Correct story list

--- stories/ScrollContainer.js
+++ stories/ScrollContainer.js
@@ -4,21 +4,21 @@ import { number } from '@storybook/addon-knobs';
 import ScrollContainer from '../src/components/ScrollContainer';
 
 storiesOf('ScrollContainer', module)
-  .addWithInfo('Default', () => (
+  .add('Default', () => (
     <div>
       <ScrollContainer height={number('height')}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag-map_of_the_world.svg/1000px-Flag-map_of_the_world.svg.png" alt="Map" />
       </ScrollContainer>
     </div>
   ))
-  .addWithInfo('Max height', () => (
+  .add('Max height', () => (
     <div>
       <ScrollContainer height={number('height', 300)}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag-map_of_the_world.svg/1000px-Flag-map_of_the_world.svg.png" alt="Map" />
       </ScrollContainer>
     </div>
   ))
-  .addWithInfo('custom theme', () => (
+  .add('custom theme', () => (
     <ScrollContainer
       height={number('height', 300)}
       theme={{
