80b15d07c61d2962678b3e898c54e7a113064cc6
Gary Thomas <gary.thomas@github.com>
2019-07-11 14:16:58 -0700

    gt - Support BS4 Spinners

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,61 +1,20 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import range from '../util/range.js';
+import Spinner from 'reactstrap/lib/Spinner';
+import ApmSpinner from './ApmSpinner';
 
-// const since these don't behave well as live props, some animation issues:
-const DURATION = '1s';
-const SEGMENTS = 12;
-
-const Spinner = ({ color, size, ...props }) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="-200 -200 200 200"
-    version="1.1"
-    {...props}
-  >
-    <defs>
-      <path id="shape" d="M20,10 A10,10 0 1 0 20,-10 L-20,-10 A10,10 0 1 0 -20,10" fill={color} />
-    </defs>
-    <style>{`
-      .gears-spinner {
-        animation: gears-spinner-spin ${DURATION} infinite steps(${SEGMENTS});
-      }
-      @keyframes gears-spinner-spin {
-        100% { transform: rotate(360deg); }
-      }
-    `}
-    </style>
-    <g transform="translate(-100,-100)">
-      <g className="gears-spinner">
-        {range(SEGMENTS).map((i) => {
-          const opacity = (i / SEGMENTS).toFixed(2);
-          const rotate = i * (360 / SEGMENTS).toFixed(2);
-
-          return (
-            <use
-              key={i}
-              xlinkHref="#shape"
-              transform={`rotate(${rotate}) translate(70, 0)`}
-              opacity={opacity}
-            />
-          );
-        })}
-      </g>
-    </g>
-  </svg>
-);
-
-Spinner.propTypes = {
-  className: PropTypes.string,
-  color: PropTypes.string,
-  size: PropTypes.string
+const SpinnerWrapper = ({ type, ...props }) => {
+  return (type === 'spin') ? <ApmSpinner {...props} /> : <Spinner type={type} {...props} />
 };
 
-Spinner.defaultProps = {
-  color: 'currentColor',
-  size: '1em'
+SpinnerWrapper.propTypes = {
+  ...Spinner.propTypes
 };
 
-export default Spinner;
+SpinnerWrapper.defaultProps = {
+  ...Spinner.defaultProps,
+  type: 'spin',
+};
 
+SpinnerWrapper.displayName = 'Spinner';
+
+export default SpinnerWrapper;
--- stories/Spinner.js
+++ stories/Spinner.js
@@ -6,24 +6,26 @@ import { Button, Spinner } from '../src';
 storiesOf('Spinner', module)
   .add('Default', () => {
     const color = select('color', ['text-primary', 'text-muted', 'text-info', 'text-success', 'text-warning', 'text-danger'], 'text-primary');
+    const type = select('type', ['spin', 'border', 'grow'], Spinner.default);
+
     return (
       <div>
         <p style={{ fontSize: `${number('fontSize', 1, { range: true, min: 1, max: 5, step: 0.25 })}rem` }}>
-          The <Spinner /> will scale with the font size of its container,
+          The <Spinner type={type} /> will scale with the font size of its container,
         </p>
 
         <hr />
         <h3>...and inherit color from it's container:</h3>
         <p>
           <Button color="secondary" size="lg" className="mr-3">
-            <Spinner /> Loading
+            <Spinner type={type} /> Loading
           </Button>
           <Button color="primary" outline size="lg">
-            <Spinner /> Loading
+            <Spinner type={type} /> Loading
           </Button>
         </p>
         <h1 className={color}>
-          {color}: <Spinner className={color} />
+          {color}: <Spinner type={type} className={color} />
         </h1>
       </div>
     );
