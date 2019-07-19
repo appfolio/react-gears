06ff11e63074748b948520f5bdb780b866307ef5
Gary Thomas <gary.thomas@github.com>
2019-07-19 08:53:22 -0700

    gt - Update Nav stories

--- stories/Nav.js
+++ stories/Nav.js
@@ -9,7 +9,11 @@ storiesOf('Nav', module)
     const activeTab = number('activeTab', 1);
     return (
       <div>
-        <Nav tabs>
+        <Nav
+          tabs
+          fill={boolean('fill', undefined)}
+          justified={boolean('justified', undefined)}
+        >
           <NavItem>
             <NavLink href="#" active={activeTab === 1} onClick={action('onClick')}>
               Receivables
@@ -43,7 +47,50 @@ storiesOf('Nav', module)
     const activeTab = number('activeTab', 1);
     return (
       <div>
-        <Nav pills vertical={boolean('vertical', false)}>
+        <Nav
+          pills
+          fill={boolean('fill', undefined)}
+          justified={boolean('justified', undefined)}
+          vertical={boolean('vertical', false)}
+        >
+          <NavItem>
+            <NavLink href="#" active={activeTab === 1} onClick={action('onClick')}>
+              Receivables
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" active={activeTab === 2} onClick={action('onClick')}>
+              Payables
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" active={activeTab === 3} onClick={action('onClick')}>
+              Bank Accounts
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" active={activeTab === 4} onClick={action('onClick')}>
+              Journal Entries
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" active={activeTab === 5} disabled onClick={action('onClick')}>
+              Disabled
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  })
+  .add('Default', () => {
+    const activeTab = number('activeTab', 1);
+    return (
+      <div>
+        <Nav
+          vertical={boolean('vertical', false)}
+          fill={boolean('fill', undefined)}
+          justified={boolean('justified', undefined)}
+        >
           <NavItem>
             <NavLink href="#" active={activeTab === 1} onClick={action('onClick')}>
               Receivables
