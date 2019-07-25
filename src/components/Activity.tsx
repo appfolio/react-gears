e41873f0a4cb25365f5460059f2097ea483fd304
Dilraj Singh <dilraj.singh@appfolio.com>
2019-07-25 10:47:54 -0700

    ds - Convert Activity to typescript

---
+++ src/components/Activity.tsx
@@ -0,0 +1,46 @@
+import React, { FunctionComponent, ReactNode } from 'react';
+import fecha from 'fecha';
+import { ListGroupItemProps } from 'reactstrap';
+import Col from './Col.js';
+import ListGroupItem from './ListGroupItem.js';
+import Row from './Row.js';
+
+interface ActivityProps extends Omit<ListGroupItemProps, 'action'> {
+  action?: ReactNode;
+  by?: ReactNode;
+  children?: ReactNode;
+  date: Date;
+  dateFormat?: string;
+}
+
+const Activity: FunctionComponent<ActivityProps> = ({
+  action,
+  by,
+  children,
+  date,
+  dateFormat = 'MM/DD/YYYY hh:mmA',
+  ...props
+}) =>
+  <ListGroupItem {...props}>
+    <Row className="w-100 no-gutters">
+      <Col xs={12} sm={4} md={3} className="text-nowrap">
+        <span>{fecha.format(date, dateFormat)}</span>
+      </Col>
+      <Col>
+        {(action || date) && (
+          <div>
+            {action && <strong>{action}</strong>}
+            {by && (
+              <span className="pl-1">
+                {action ? 'by ' : ''}
+                <span>{by}</span>
+              </span>
+            )}
+          </div>
+        )}
+        {children}
+      </Col>
+    </Row>
+  </ListGroupItem>;
+
+export default Activity;
