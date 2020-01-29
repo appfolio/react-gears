7ecd531a07301eb2e469c82fbd5bc690079481a8
Phoebe <phgao1994@gmail.com>
2020-01-28 16:49:05 -0800

    use cards in dnd card grid live example

--- stories/DragAndDropGrid.js
+++ stories/DragAndDropGrid.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { DragAndDropGrid } from '../src/index';
+import { DragAndDropGrid, Card, CardBody, CardHeader } from '../src/index';
 
 storiesOf('DragAndDropGrid', module)
   .add('Live example', () => {
     let items = [
-      [{ id: 1, content: 'ice bear' }, { id: 2, content: 'panda' }],
-      [{ id: 3, content: 'grizz' }, { id: 4, content: 'nomnom' }],
+      [
+        { id: 1, content: <Card><CardHeader>Ice Bear</CardHeader><CardBody>Ice bear has decided. Not into hibernation.</CardBody></Card> },
+        { id: 2, content: <Card><CardHeader>Panda</CardHeader><CardBody>I'm not fat. I just love to eat.</CardBody></Card> },
+        { id: 3, content: <Card><CardHeader>Grizz</CardHeader><CardBody>Hello, my name is Grizzly! We're here to be internet famous.</CardBody></Card> },
+      ],
+      [
+        { id: 4, content: <Card><CardHeader>Charlie</CardHeader><CardBody>No way... Cheese Poofies? How'd you know they're my favorite snack!?</CardBody></Card> },
+        { id: 5, content: <Card><CardHeader>Nom Nom</CardHeader><CardBody>GET OUT OF THE WAY! CUT!</CardBody></Card> },
+        { id: 6, content: <Card><CardHeader>Chloe Park</CardHeader><CardBody>Actually, it's a college course. I skipped a couple of grades, so the jokes on you!</CardBody></Card> }
+      ],
     ];
 
     const grid = 8;
 
-    const getListStyle = (isDraggingOver) => {
+    const getListStyle = () => {
       return {
-        background: isDraggingOver ? 'lightblue' : 'lightgrey',
         padding: grid,
         overflow: 'auto',
       };
     };
 
-    const getItemStyle = (isDragging) => {
+    const getItemStyle = () => {
       return {
         // some basic styles to make the items look a bit nicer
         userSelect: 'none',
         padding: grid * 2,
         margin: `0 ${grid}px 0 0`,
-
-        // change background colour if dragging
-        background: isDragging ? 'lightgreen' : 'grey',
       };
     };
 
