58b0744d404ef82fdfd8ce7aa00c2c3e13dea865
Phoebe <phgao1994@gmail.com>
2020-02-04 10:18:29 -0800

    Add React.FunctionComponent Type to Masked Input

--- src/components/MaskedInput.tsx
+++ src/components/MaskedInput.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import MaskedInputBase, { MaskedInputProps } from 'react-text-mask';
 
-const MaskedInput = ({ guide = false, ...props }: MaskedInputProps) => (
+const MaskedInput: React.FunctionComponent<MaskedInputProps> = (
+  { guide = false, ...props }: MaskedInputProps
+) => (
   <MaskedInputBase className="form-control" guide={guide} {...props} />
 );
 
