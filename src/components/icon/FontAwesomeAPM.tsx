846f81c8c2612f64b215023e6aeb4be283fdf626
Phoebe Gao <phoebe.gao@appfolio.com>
2020-02-11 13:10:25 -0800

    expose style property for Icon component

--- src/components/icon/FontAwesomeAPM.tsx
+++ src/components/icon/FontAwesomeAPM.tsx
@@ -27,6 +27,7 @@ export interface FontAwesomeAPMProps {
   size?: 'lg' | '2x' | '3x' | '4x' | '5x';
   spin?: boolean;
   stack?: '1x' | '2x';
+  style?: React.CSSProperties;
   tag?: keyof JSX.IntrinsicElements;
 }
 
