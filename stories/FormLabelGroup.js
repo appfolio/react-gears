b54b39320b05c5396a2ab3650e447c40e6cce605
Quan Chau <quan.chau@appfolio.com>
2020-02-11 09:44:16 -0800

    qc - demonstrate problem with HelpBubble in FormLabelGroup

--- stories/FormLabelGroup.js
+++ stories/FormLabelGroup.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, object, text, select } from '@storybook/addon-knobs';
-import { Alert, FormLabelGroup } from '../src';
+import { CheckboxInput, Alert, FormLabelGroup, HelpBubble } from '../src';
 
 storiesOf('FormLabelGroup', module)
-  .add('Live example', () => (
-    <div>
+  .add('Live example', () => {
+    const { checked, setChecked } = useState(false);
+
+    return <div>
       <FormLabelGroup
-        label={text('label', 'Some Input')}
+        label={<Fragment><span>Should eat cake?</span><HelpBubble className="ml-1" placement="top" title="" >Cake will make you fat though.</HelpBubble></Fragment>}
         labelSize={select('labelSize', ['sm', 'md', 'lg'], 'md')}
         feedback={text('feedback', 'You must give a first name')}
         validFeedback={text('validFeedback')}
@@ -16,10 +18,15 @@ storiesOf('FormLabelGroup', module)
         required={boolean('required', false)}
         inline={boolean('inline', false)}
         stacked={boolean('stacked', false)}
+        inputId="should-eat-cake"
       >
-        <Alert color="info" className="text-center p-4 mb-0" style={{ borderStyle: 'dashed' }}>
-          Your content here
-        </Alert>
+
+        <CheckboxInput
+          type='checkbox'
+          id='should-eat-cake'
+          onChange={() => setChecked(!checked)}
+          value={checked}
+        />
       </FormLabelGroup>
-    </div>
-  ));
+    </div>;
+  });
