bdc9273b6be91219060733d2e91da56075520ec3
Gary Thomas <gary.thomas@appfolio.com>
2020-03-09 07:31:57 -0700

    gt - Update theme list

--- .storybook/themes.js
+++ .storybook/themes.js
@@ -1,15 +1,16 @@
 export default {
   APM: [
     { name: 'APM/Saffron', url: 'https://appfolio.github.io/bootstrap-saffron-4/bootstrap-saffron.css' },
-    { name: 'APM/Cayenne', url: 'https://appfolio.github.io/bootstrap-cayenne/bootstrap-cayenne.css' },
     { name: 'APM/Coastline', url: 'https://appfolio.github.io/bootstrap-coastline/bootstrap-coastline.css' },
+    { name: 'APM/Coastline Dark', url: 'https://apm-prod-frontend-assets.s3.amazonaws.com/styles/bootstrap-coastline-dark.min.css' },
     { name: 'APM/Listings', url: 'https://appfolio.github.io/bootstrap-listings/bootstrap-listings.css' },
-    { name: 'APM/OPortal', url: 'https://appfolio.github.io/bootstrap-oportal/bootstrap-oportal.css' },
-    { name: 'APM/Dream', url: 'https://appfolio.github.io/bootstrap-dream/bootstrap-dream.css' },
-    { name: 'APM/Admin', url: 'https://appfolio.github.io/bootstrap-admin/bootstrap-admin.css' }
+    { name: 'APM/Admin', url: 'https://appfolio.github.io/bootstrap-admin/bootstrap-admin.css' },
   ],
   MyCase: [
     { name: 'MyCase Focus', url: 'https://appfolio.github.io/bootstrap-mycase-focus/bootstrap-mycase-focus.css' }
+  ],
+  IM: [
+    { name: 'GP', url: 'https://s3-us-east-2.amazonaws.com/af-im-ui/branches/master/gp/style.css' },
   ],
   Bootstrap: [
     { name: 'Default', url: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' },
