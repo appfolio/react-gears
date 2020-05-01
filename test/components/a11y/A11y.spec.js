bed2e1385ffd0c684ab9116cd5e780fe55186afd
Darren Eng <darren.eng@appfolio.com>
2020-05-01 11:55:56 -0700

    de - replace enzyme with rtl in a11y tests
    - RTL treats a11y as a first class concern
    - Exposes built in `cleanup` function
      `axe-core` is framework agnostic so we needed to attach the component
      to a DOM node and then remove it. Not needing to write this ourselves
      is a plus.

--- test/components/a11y/A11y.spec.js
+++ test/components/a11y/A11y.spec.js
@@ -1,27 +1,21 @@
 import assert from 'assert';
 import axe from 'axe-core';
-import { mount } from 'enzyme';
+import { render, cleanup } from '@testing-library/react';
 import React from 'react';
 import { Activity, ActivityLog, AddressInput, Alert } from '../../../src';
 
-async function testComponent(element) {
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-
-  mount(element, { attachTo: div });
-  const { violations } = await axe.run(div);
-  assert.deepStrictEqual(violations, []);
-
-  document.body.removeChild(div);
-}
-
 describe('Accessibility Tests', () => {
+  afterEach(cleanup);
+
   it('should be accessible for ActivityLog', async () => {
-    await testComponent(
+    const { container } = render(
       <ActivityLog>
         <Activity date={new Date()} action="Edited" by="Jane Doe" />
         <Activity date={new Date()} action="Created" by="John Doe" />
       </ActivityLog>
     );
+    const { violations } = await axe.run(container);
+    assert.deepStrictEqual(violations, []);
+  });
   });
 });
