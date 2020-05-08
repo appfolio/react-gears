ed4c2566362506ea050797e56d8ec8c818198c43
Gary Thomas <gary.thomas@appfolio.com>
2020-05-07 17:35:42 -0700

    gt - Move hook to ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import createPersistedState from 'use-persisted-state';
 
-function useLocalStorage(key, value) {
+function useLocalStorage<T>(key: string, value: T) {
   const usePersistedState = useMemo(() => createPersistedState(key), [key]);
 
   return usePersistedState(value);
