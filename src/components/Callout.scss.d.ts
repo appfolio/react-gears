fe48f860f4f95e49327b4629900f285a894584ca
Phoebe Gao <phoebe.gao@appfolio.com>
2020-06-04 17:16:21 -0700

    pg - convert Callout

--- src/components/Callout.scss.d.ts
+++
@@ -1,7 +0,0 @@
-export const callout: string;
-export const body: string;
-export const arrow: string;
-export const top: string;
-export const right: string;
-export const bottom: string;
-export const left: string;
