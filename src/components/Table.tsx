fdb7de3ce8a366e654c956b0b8d6a1bec9190156
Yenju Lee <yenju.lee@appfolio.com>
2020-06-12 15:08:27 -0700

    yl - Add back TableProps to Table.tsx
    Also export TableProps since it is used in SortableTable.d.ts.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,19 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import TableComponent from 'reactstrap/lib/Table';
 
-class Table extends React.Component {
+export interface TableProps {
+  size?: string;
+  bordered?: boolean;
+  borderless?: boolean;
+  striped?: boolean;
+  dark?: boolean;
+  hover?: boolean;
+  responsive?: boolean;
+}
+
+class Table extends React.Component<TableProps, {}> {
   static displayName = 'Table';
-  static propTypes = {
-    size: PropTypes.string,
-    bordered: PropTypes.bool,
-    borderless: PropTypes.bool,
-    striped: PropTypes.bool,
-    dark: PropTypes.bool,
-    hover: PropTypes.bool,
-    responsive: PropTypes.bool,
-    children: PropTypes.node
-  }
+
   static defaultProps = {
     hover: true,
     responsive: true,
