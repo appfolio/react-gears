08e37107d56e4a811474dcd254ff8400213f7d4c
Yenju Lee <yenju.lee@appfolio.com>
2020-06-15 22:23:56 -0700

    yl - Convert CheckboxListInput to functional component

--- src/components/CheckboxListInput.tsx
+++ src/components/CheckboxListInput.tsx
@@ -6,11 +6,14 @@ interface CheckboxListInputProps {
   value?: string[],
 }
 
-class CheckboxListInput extends React.Component<CheckboxListInputProps, {}> {
-  onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (this.props.onChange) {
+const CheckboxListInput: React.FunctionComponent<CheckboxListInputProps> = ({
+  children,
+  ...props
+}) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (props.onChange) {
       const { checked, value } = e.target;
-      const currentSelection = (this.props.value || []).slice(0);
+      const currentSelection = (props.value || []).slice(0);
 
       if (checked) {
         currentSelection.push(value);
@@ -19,29 +22,20 @@ class CheckboxListInput extends React.Component<CheckboxListInputProps, {}> {
         i > -1 && currentSelection.splice(i, 1); // eslint-disable-line no-unused-expressions
       }
 
-      this.props.onChange(currentSelection);
+      props.onChange(currentSelection);
     }
   };
 
-  render() {
-    const {
-      value,
-      children,
-      onChange,
-      ...props
-    } = this.props;
-
-    return (
-      <div>
-        {React.Children.map(children as React.ReactElement | React.ReactElement[], choice => React.cloneElement(choice, {
-          type: 'checkbox',
-          selected: value,
-          onChange: this.onChange,
-          ...props
-        }))}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {React.Children.map(children as React.ReactElement | React.ReactElement[], choice => React.cloneElement(choice, {
+        type: 'checkbox',
+        selected: props.value,
+        onChange: onChange,
+        ...props
+      }))}
+    </div>
+  );
+};
 
 export default CheckboxListInput;
