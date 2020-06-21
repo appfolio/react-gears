6cdc8d6d1e249527abf77cb837d2c8a04d6e6f39
Yenju Lee <yenju.lee@appfolio.com>
2020-06-21 16:23:47 -0700

    yl - Convert Paginator/Page to func comp

--- src/components/Paginator/Page.tsx
+++ src/components/Paginator/Page.tsx
@@ -1,31 +1,38 @@
-// @ts-nocheck
 import React from 'react';
 import PropTypes from 'prop-types';
 import PaginationItem from '../PaginationItem';
 import PaginationLink from '../PaginationLink';
 
+type PageProps = {
+  current: boolean,
+  onClick: (pageNumber: number) => void,
+  page: number,
+}
+
 /**
  * A clickable link to a page in the pagination bar
  */
-export default class Page extends React.Component {
-  static propTypes = {
-    current: PropTypes.bool.isRequired,
-    onClick: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-  }
-
-  onClick = (event) => {
+const Page: React.FunctionComponent<PageProps> = ({
+  current,
+  page,
+  ...props
+}) => {
+  const onClick = (event: React.MouseEvent) => {
     event.preventDefault();
-    this.props.onClick(this.props.page);
-  }
+    props.onClick(page);
+  };
 
-  render() {
-    const { current, page } = this.props;
+  return (
+    <PaginationItem active={current}>
+      <PaginationLink onClick={onClick}>{page}</PaginationLink>
+    </PaginationItem>
+  );
+};
 
-    return (
-      <PaginationItem active={current}>
-        <PaginationLink onClick={this.onClick}>{page}</PaginationLink>
-      </PaginationItem>
-    );
-  }
-}
+Page.propTypes = {
+  current: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+};
+
+export default Page;
