5a2f9db1559449970fdf73a93072398b027ae3d3
Yenju Lee <yenju.lee@appfolio.com>
2020-06-21 17:31:24 -0700

    yl - Convert Note to functional component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -31,81 +31,93 @@ type NoteProps = {
   savingLabel?: React.ReactNode
 }
 
-class Note extends React.Component<NoteProps, {}> {
-  static displayName = 'Note';
+const Note: React.FunctionComponent<NoteProps> = ({
+  children,
+  className,
+  dateFormat,
+  note,
+  onCancel,
+  onChange,
+  onDelete,
+  onEdit,
+  onSave,
+  onUndelete,
+  rows,
+  saving,
+  saveLabel,
+  savingLabel,
+  showTimezone
+}) => {
+  const { deleted, editing, text } = note;
 
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    dateFormat: PropTypes.string,
-    showTimezone: PropTypes.bool,
-    note: PropTypes.shape({
-      deleted: PropTypes.bool,
-      editing: PropTypes.bool,
-      text: PropTypes.string.isRequired
-    }).isRequired,
-    onCancel: PropTypes.func,
-    onChange: PropTypes.func,
-    onDelete: PropTypes.func,
-    onEdit: PropTypes.func,
-    onSave: PropTypes.func,
-    onUndelete: PropTypes.func,
-    rows: PropTypes.number,
-    saving: PropTypes.bool,
-    saveLabel: PropTypes.node,
-    savingLabel: PropTypes.node
-  };
-
-  static defaultProps = {
-    className: 'bg-white mb-3',
-    dateFormat: 'ddd, MMMM D, YYYY "at" h:mm A',
-    showTimezone: true,
-    // @ts-ignore
-    rows: EditableNote.defaultProps.rows,
-    // @ts-ignore
-    saving: EditableNote.defaultProps.saving,
-    // @ts-ignore
-    saveLabel: EditableNote.defaultProps.saveLabel,
-    // @ts-ignore
-    savingLabel: EditableNote.defaultProps.savingLabel,
-  };
-
-  render() {
-    const { children, className, dateFormat, note, onCancel, onChange, onDelete, onEdit, onSave, onUndelete, rows, saving, saveLabel, savingLabel, showTimezone }
-      = this.props;
-    const { deleted, editing, text } = note;
-
-    if (deleted) {
-      return (
-        <DeletedNote
-          className={className}
-          note={note}
-          onUndelete={onUndelete}
-        />);
-    } else if (editing) {
-      return (
-        <EditableNote
-          className={className}
-          note={note}
-          onCancel={onCancel}
-          onChange={onChange}
-          onSave={onSave}
-          rows={rows}
-          saving={saving}
-          saveLabel={saveLabel}
-          savingLabel={savingLabel}
-        />);
-    }
+  if (deleted) {
     return (
-      <Card className={className}>
-        <NoteHeader note={note} dateFormat={dateFormat} showTimezone={showTimezone} onDelete={onDelete} onEdit={onEdit} />
-        <CardBody>
-          <CardText style={{ whiteSpace: 'pre-wrap' }}>{text}</CardText>
-          {children}
-        </CardBody>
-      </Card>
-    );
+      <DeletedNote
+        className={className}
+        note={note}
+        onUndelete={onUndelete}
+      />);
+  } else if (editing) {
+    return (
+      <EditableNote
+        className={className}
+        note={note}
+        onCancel={onCancel}
+        onChange={onChange}
+        onSave={onSave}
+        rows={rows}
+        saving={saving}
+        saveLabel={saveLabel}
+        savingLabel={savingLabel}
+      />);
   }
-}
+  return (
+    <Card className={className}>
+      <NoteHeader note={note} dateFormat={dateFormat} showTimezone={showTimezone} onDelete={onDelete} onEdit={onEdit} />
+      <CardBody>
+        <CardText style={{ whiteSpace: 'pre-wrap' }}>{text}</CardText>
+        {children}
+      </CardBody>
+    </Card>
+  );
+};
+
+Note.displayName = 'Note';
+
+Note.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  dateFormat: PropTypes.string,
+  showTimezone: PropTypes.bool,
+  note: PropTypes.shape({
+    deleted: PropTypes.bool,
+    editing: PropTypes.bool,
+    text: PropTypes.string.isRequired
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onDelete: PropTypes.func,
+  onEdit: PropTypes.func,
+  onSave: PropTypes.func.isRequired,
+  onUndelete: PropTypes.func,
+  rows: PropTypes.number,
+  saving: PropTypes.bool,
+  saveLabel: PropTypes.node,
+  savingLabel: PropTypes.node
+};
+
+Note.defaultProps = {
+  className: 'bg-white mb-3',
+  dateFormat: 'ddd, MMMM D, YYYY "at" h:mm A',
+  showTimezone: true,
+  // @ts-ignore
+  rows: EditableNote.defaultProps.rows,
+  // @ts-ignore
+  saving: EditableNote.defaultProps.saving,
+  // @ts-ignore
+  saveLabel: EditableNote.defaultProps.saveLabel,
+  // @ts-ignore
+  savingLabel: EditableNote.defaultProps.savingLabel,
+};
 
 export default Note;
