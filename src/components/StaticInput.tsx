c4f612b1aa4072d480006dd5b32f7b5516fa67e8
Justin Maher <justin.maher@appfolio.com>
2020-07-24 08:31:46 -0700

    migrate StaticInput to typescript

---
+++ src/components/StaticInput.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from 'react';
+import { InputProps } from 'reactstrap/lib/Input';
+
+import Input from './Input';
+
+export interface StaticInputProps extends InputProps {
+  children?: React.ReactNode;
+}
+
+const StaticInput: FunctionComponent<InputProps> = ({
+  children,
+  ...props
+}) => (
+  <Input
+    {...props}
+    type="text"
+    plaintext
+  />
+);
+
+StaticInput.defaultProps = {
+  ...Input.defaultProps
+};
+
+export default StaticInput;
