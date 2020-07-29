7ef56374b395b55f884cfae8f0ce25972fbf7de6
Gary Thomas <gary.thomas@appfolio.com>
2020-07-29 16:43:58 -0700

    gt - Updates for move to TS

--- src/components/Waiting.tsx
+++ src/components/Waiting.tsx
@@ -1,10 +1,7 @@
 import React, { ReactNode, FunctionComponent } from 'react';
-import classnames from 'classnames';
 import { ModalProps } from 'reactstrap/lib/Modal';
 import Modal from './Modal';
 import Spinner from './Spinner';
-
-import styles from './Waiting.scss';
 
 const noop = () => {};
 
@@ -24,21 +21,27 @@ const Waiting: FunctionComponent<WaitingProps> = ({
   className,
   title = 'Please Wait',
   ...props
-}) => {
-  const spinnerClasses = classnames('text-white', styles.spinner);
-  return (
-    <Modal {...props} className={classnames(styles.waiting, className)} toggle={noop}>
-      {title ?
-        <header className="text-center text-white px-4 pt-4">
-          {title}
-        </header>
-        : null}
-      <div className="text-center p-4">
-        {children || <Spinner className={spinnerClasses} />}
-      </div>
-    </Modal>
-  );
-};
+}) => (
+  <Modal
+    {...props}
+    className={className}
+    contentClassName="bg-dark border-0 text-center text-light"
+    toggle={noop}
+    style={{
+      margin: '40vh auto',
+      width: '9rem'
+    }}
+  >
+    {title ?
+      <header className="px-4 pt-4">
+        {title}
+      </header>
+      : null}
+    <div className="p-4">
+      {children || <Spinner style={{ fontSize: '30px' }} />}
+    </div>
+  </Modal>
+);
 
 Waiting.defaultProps = {
   title: 'Please Wait',
