d70005c837a3e4d696980525f082ecd9cb139274
David <david.acevedo@appfolio.com>
2020-09-30 15:45:04 -0700

    feat: fun hacks

--- test/components/Select.spec.js
+++ test/components/Select.spec.js
@@ -16,7 +16,7 @@ const OPTIONS = [
 describe('<Select />', () => {
   describe('uncontrolled', () => {
     describe('without defaultValue', () => {
-      const component = shallow(<Select options={OPTIONS} />).childAt(0);
+      const component = shallow(<Select options={OPTIONS} />);
 
       it('should have a blank default', () => {
         assert.equal(component.type(), ReactSelect);
@@ -34,18 +34,18 @@ describe('<Select />', () => {
       const component = shallow(<Select options={OPTIONS} defaultValue={2} />);
 
       it('should start with default', () => {
-        assert.equal(component.childAt(0).prop('value'), 2);
+        assert.equal(component.prop('value'), 2);
       });
 
       it('should update the value when changed', () => {
-        component.childAt(0).simulate('change', 'stuff');
-        assert.equal(component.childAt(0).prop('value'), 'stuff');
+        component.simulate('change', 'stuff');
+        assert.equal(component.prop('value'), 'stuff');
       });
     });
 
     it('should call additional onChange', () => {
       const callback = sinon.spy();
-      const component = shallow(<Select options={OPTIONS} onChange={callback} />).childAt(0);
+      const component = shallow(<Select options={OPTIONS} onChange={callback} />);
       component.simulate('change', 'stuff');
 
       assert(callback.calledOnce);
@@ -54,7 +54,7 @@ describe('<Select />', () => {
   });
 
   describe('controlled', () => {
-    const component = shallow(<Select options={OPTIONS} value={3} defaultValue={2} />).childAt(0);
+    const component = shallow(<Select options={OPTIONS} value={3} defaultValue={2} />);
 
     it('should render with the given value', () => {
       assert.equal(component.prop('value'), 3);
@@ -77,7 +77,7 @@ describe('<Select />', () => {
       });
     };
 
-    const component = shallow(<Select loadOptions={getOptions} />).childAt(0);
+    const component = shallow(<Select loadOptions={getOptions} />);
     assert.equal(component.type(), ReactSelect.Async);
     assert(component.hasClass('select-async'));
   });
@@ -93,7 +93,7 @@ describe('<Select />', () => {
       });
     };
 
-    const component = shallow(<Select loadOptions={getOptions} className="foo bar" />).childAt(0);
+    const component = shallow(<Select loadOptions={getOptions} className="foo bar" />);
     assert.equal(component.type(), ReactSelect.Async);
     assert(component.hasClass('foo'));
     assert(component.hasClass('bar'));
@@ -121,22 +121,22 @@ describe('<Select />', () => {
   it('should render AsyncCreatable if loadOptions and creatable', () => {
     const getOptions = () => {};
 
-    const component = shallow(<Select loadOptions={getOptions} creatable />).childAt(0);
+    const component = shallow(<Select loadOptions={getOptions} creatable />);
     assert.equal(component.type(), ReactSelect.AsyncCreatable);
   });
 
   it('should render Creatable if creatable', () => {
-    const component = shallow(<Select creatable />).childAt(0);
+    const component = shallow(<Select creatable />);
     assert.equal(component.type(), ReactSelect.Creatable);
   });
 
   it('should pass name prop', () => {
-    const component = mount(<Select name="Yowza" />).childAt(0);
+    const component = mount(<Select name="Yowza" />);
     assert(component.find('input[name="Yowza"]').exists());
   });
 
   it('should pass inputProps name prop', () => {
-    const component = mount(<Select inputProps={{ name: 'Gonzo' }} />).childAt(0);
+    const component = mount(<Select inputProps={{ name: 'Gonzo' }} />);
     assert(component.find('input[name="Gonzo"]').exists());
   });
 });
