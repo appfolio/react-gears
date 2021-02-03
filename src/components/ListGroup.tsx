9c2c498fa1fca2fcb1552eb09cf9f9a1da64ed45
Gary Thomas <gary.thomas@appfolio.com>
2021-02-02 17:11:50 -0800

    gt - Correct TS Props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,12 @@ import React, { FunctionComponent } from 'react';
 import { ListGroup as ListGroupComponent, ListGroupProps } from 'reactstrap';
 import classnames from 'classnames';
 
-type Props = {
-  striped?: boolean
-} & ListGroupProps;
+interface Props extends ListGroupProps {
+  className?: string;
+  striped?: boolean;
+}
 
-const ListGroup: FunctionComponent<Props> = ({
-  className,
-  striped,
-  ...props
-}) => (
+const ListGroup: FunctionComponent = ({ className, striped, ...props }: Props) => (
   <ListGroupComponent
     className={classnames(className, { 'list-group-striped': striped })}
     {...props}
