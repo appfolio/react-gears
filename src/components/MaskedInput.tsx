5799ec0143793e34e36925a0071a5c3709621826
Phoebe Gao <phoebe.gao@appfolio.com>
2021-04-14 15:02:02 -0700

    remove react-imask.d.ts imports
    type declarations are ambient and don't need to be imported

--- src/components/MaskedInput.tsx
+++ src/components/MaskedInput.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { IMaskInput, IMaskInputProps } from 'react-imask';
 import classnames from 'classnames';
 import IMask from 'imask';
-// eslint-disable-next-line import/no-unresolved
-import './TypeHelpers/react-imask.d.ts';
 
 function MaskedInput<MaskOptions extends IMask.AnyMaskedOptions>({ className: classes, ...props }: IMaskInputProps<MaskOptions>) {
   const className = classnames('form-control', classes);
