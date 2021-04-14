db79dc9d4c964a0829db50e60fee16099c98a29c
Phoebe Gao <phoebe.gao@appfolio.com>
2021-04-13 19:04:02 -0700

    update MaskedInput

--- stories/MaskedInput.stories.js
+++ stories/MaskedInput.stories.js
@@ -7,13 +7,27 @@ export default {
   component: MaskedInput,
 };
 
+const imaskLink = <a href="https://imask.js.org/">imask.js</a>;
+const reactTextMaskLink = <a href="https://text-mask.github.io/text-mask/">react-text-mask</a>;
+
+export const Introduction = () => (
+  <section>
+    <p>
+      <code>MaskedInput</code> is a react component that uses {imaskLink} masking under the hood.{' '}
+      All <code>imask</code> options can be passed to the <code>MaskedInput</code> components via props.{' '}
+      React Gears 6 and earlier versions used {reactTextMaskLink}, but we migrated to{' '}
+      {imaskLink} since {reactTextMaskLink} is no longer maintained.
+    </p>
+
+    <p>Please review the <a href="https://imask.js.org/guide.html">imask.js guide</a> for detailed documentation.</p>
+  </section>
+);
+
 export const PhoneNumber = () => (
   <MaskedInput
+    mask="(000)000-0000"
+    lazy={boolean('lazy', true)}
     placeholder={text('placeholder', '(555) 495-3947')}
-    mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-    guide={boolean('guide', false)}
-    placeholderChar={text('placeholderChar', undefined)}
-    keepCharPositions={boolean('keepCharPositions', false)}
-    showMask={boolean('showMask', false)}
+    placeholderChar={text('placeholderChar', '0')}
   />
 );
