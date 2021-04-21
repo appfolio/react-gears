96ca07e051345db965b9410325d91130d7c757d4
Phoebe Gao <phoebe.gao@appfolio.com>
2021-04-20 17:53:54 -0700

    add support for multiselect

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { findDOMNode } from 'react-dom';
 import { DropdownProps, InputProps } from 'reactstrap';
+import Badge from './Badge';
 import Button from './Button';
 import Dropdown from './Dropdown';
 import DropdownItem from './DropdownItem';
@@ -13,51 +14,56 @@ import InputGroupAddon from './InputGroupAddon';
 
 type Direction = 'up' | 'down';
 
-type Option = {
+type Option<T> = {
   label: string;
-  value: any;
+  value: T;
   disabled?: boolean;
 }
 
-interface ComboboxProps extends InputProps {
-  options: Option[];
+interface BaseComboboxProps<T> extends Omit<InputProps, 'onChange'> {
+  options: Option<T>[];
   direction?: Direction;
   dropdownProps?: DropdownProps;
   noResultsLabel?: string;
-  onChange?: (value: any) => void;
-  filterOptions?: (options: Option[], value: any) => Option[];
-  renderInputValue?: (option: Option) => string;
-  renderOption?: (option: Option) => React.ReactNode;
+  onChange?: (value?: T | T[]) => void;
+  filterOptions?: (options: Option<T>[], value: string) => Option<T>[];
+  renderInputValue?: (option: Option<T>) => string;
+  renderOption?: (option: Option<T>) => React.ReactNode;
   menuMaxHeight?: string;
 }
+
+interface MultiComboboxProps<T> extends BaseComboboxProps<T> {
+  multi: true
+}
+
+type ComboboxProps<T> = BaseComboboxProps<T> | MultiComboboxProps<T>;
 
 const defaultProps = {
   noResultsLabel: 'No results found',
   onChange: () => {},
-  filterOptions: (o: Option[], v: any) => o.filter(option => v ? option.label.toLowerCase().indexOf(v.toLowerCase()) > -1 : true),
-  renderInputValue: (option: Option) => option.label,
-  renderOption: (option: Option) => option.label,
+  filterOptions: (o: Option<any>[], v: any) => o.filter(option => v ? option.label.toLowerCase().indexOf(v.toLowerCase()) > -1 : true),
+  renderInputValue: (option: Option<any>) => option.label,
+  renderOption: (option: Option<any>) => option.label,
 };
 
-const Combobox: React.FunctionComponent<ComboboxProps> = ({
-  className, direction, disabled, dropdownProps, inputClassName, options, placeholder, value, menuMaxHeight,
+function Combobox<T>({
+  className, direction, disabled, dropdownProps, inputClassName, options, placeholder, value, menuMaxHeight, multi,
   noResultsLabel = defaultProps.noResultsLabel,
   onChange = defaultProps.onChange,
   filterOptions = defaultProps.filterOptions,
   renderInputValue = defaultProps.renderInputValue,
   renderOption = defaultProps.renderOption,
   ...props
-}) => {
+}: ComboboxProps<T>) {
   const [open, setOpen] = useState(false);
   const [inputValue, setInputValue] = useState('');
-  const [visibleOptions, setVisibleOptions] = useState<Option[]>([]);
+  const [visibleOptions, setVisibleOptions] = useState<Option<T>[]>([]);
   const [focusedOptionIndex, setFocusedOptionIndex] = useState<number>(0);
 
   const inputElement = useRef<HTMLInputElement>(null);
   const dropdownMenu = useRef(null);
   const focusedOption = useRef(null);
-
-  const selectedOption = options.find(option => option.value === value);
+  const selected = useMemo<Option<T> | Option<T>[]>(() => multi ? (value || []).map((v: T) => options.find(option => option.value === v)) : options.find(option => option.value === value), [value, options, multi]);
 
   useEffect(() => {
     if (visibleOptions.length > 0) {
@@ -67,15 +73,21 @@ const Combobox: React.FunctionComponent<ComboboxProps> = ({
 
   useEffect(() => {
     setInputValue('');
-  }, [selectedOption, open]);
+  }, [selected, open]);
 
   useEffect(() => {
     if (!open && inputElement.current) inputElement.current.blur();
   }, [open]);
 
   useEffect(() => {
-    setVisibleOptions(filterOptions(options, inputValue));
-  }, [inputValue, setVisibleOptions, filterOptions, options]);
+    let selectableOptions = [...options];
+
+    if (multi && value) {
+      selectableOptions = options.filter(o => value.indexOf(o.value) === -1);
+    }
+
+    setVisibleOptions(filterOptions(selectableOptions, inputValue));
+  }, [inputValue, setVisibleOptions, filterOptions, options, multi, value]);
 
   const scrollFocusedOptionIntoView = () => {
     if (dropdownMenu.current === null || focusedOption.current === null) return;
@@ -100,9 +112,22 @@ const Combobox: React.FunctionComponent<ComboboxProps> = ({
 
   useEffect(scrollFocusedOptionIntoView, [focusedOptionIndex]);
 
-  const selectOption = (option: Option) => {
+  const selectOption = (option: Option<T>) => {
+    if (multi) {
+      onChange([...(value || []), option.value]);
+      return;
+    }
     onChange(option.value);
     setOpen(false);
+  };
+
+  const removeOption = (option: Option<T>) => {
+    const i = value.indexOf(option.value);
+    if (i > -1) {
+      const newValue = [...value];
+      newValue.splice(i, 1);
+      onChange(newValue);
+    }
   };
 
   const handleOptionsKeyboardNav = ({ key }: React.KeyboardEvent<HTMLElement>) => {
@@ -114,79 +139,98 @@ const Combobox: React.FunctionComponent<ComboboxProps> = ({
       setFocusedOptionIndex(focusedOptionIndex + 1);
     } else if (key === 'ArrowUp' && focusedOptionIndex > 0) {
       setFocusedOptionIndex(focusedOptionIndex - 1);
-    } else if (selectedOption && key === 'Backspace' && inputValue === '') {
+    } else if (selected && key === 'Backspace' && inputValue === '') {
+      if (multi) {
+        const newValue = [...value];
+        newValue.pop();
+        onChange(newValue);
+        return;
+      }
       onChange(undefined);
     }
   };
 
   return (
-    <Dropdown
-      direction={direction}
-      isOpen={!disabled && open}
-      toggle={() => {}}
-      onBlur={() => { setOpen(false); }}
-    >
-      <DropdownToggle tag="div" disabled={disabled}>
-        { selectedOption && inputValue === '' &&
-          // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-          <div
-            aria-label="Selected value"
-            className="py-2 px-3"
-            style={{ position: 'absolute', width: 'calc(100% - 1.5rem)', zIndex: 4, left: 1 }}
-            onMouseDown={(ev) => {
-              ev.preventDefault();
-              if (inputElement.current) inputElement.current.focus();
-            }}
-          >
-            {selectedOption && renderInputValue(selectedOption)}
-          </div>
+    <>
+      <div className="d-flex flex-wrap mb-2">
+        {
+          multi &&
+          (selected as Option<T>[]).map((o: Option<T>) => (
+            <Button color="" className="btn-sm p-0 mr-1" onClick={() => removeOption(o)}>
+              <Badge key={`${o.value}`} style={{ textTransform: 'none' }} className="p-2">
+                {o.label}{' '} <Icon name="close" />
+              </Badge>
+            </Button>
+          ))
         }
-        <InputGroup className={className}>
-          <Input
-            innerRef={inputElement}
-            data-testid="combobox-input"
-            disabled={disabled}
-            className={inputClassName}
-            placeholder={selectedOption ? undefined : placeholder}
-            onFocus={(ev) => {
+      </div>
+      <Dropdown
+        direction={direction}
+        isOpen={!disabled && open}
+        toggle={() => {}}
+        onBlur={() => { setOpen(false); }}
+      >
+        <DropdownToggle tag="div" disabled={disabled}>
+          <InputGroup className={className}>
+            { !multi && selected && inputValue === '' &&
+              // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+              <div
+                aria-label="Selected value"
+                className="py-2 px-3"
+                style={{ position: 'absolute', width: 'calc(100% - 1.5rem)', zIndex: 4, left: 1 }}
+                onMouseDown={(ev) => {
+                  ev.preventDefault();
+                  if (inputElement.current) inputElement.current.focus();
+                }}
+              >
+                {renderInputValue((selected as Option<T>))}
+              </div>
+            }
+            <Input
+              innerRef={inputElement}
+              data-testid="combobox-input"
+              disabled={disabled}
+              className={inputClassName}
+              placeholder={selected ? undefined : placeholder}
+              onFocus={(ev) => {
               ev.preventDefault();
               ev.stopPropagation();
 
               setOpen(true);
             }}
-            onChange={(e) => {
+              onChange={(e) => {
               e.preventDefault();
               e.stopPropagation();
               setInputValue(e.target.value);
             }}
-            onKeyDown={handleOptionsKeyboardNav}
-            type="search"
-            value={inputValue}
-            {...props}
-          />
-          <InputGroupAddon addonType="append">
-            <Button
-              className="px-2"
-              data-testid="combobox-caret"
-              disabled={disabled}
-              active={open}
-              onMouseDown={(ev) => {
+              onKeyDown={handleOptionsKeyboardNav}
+              type="search"
+              value={inputValue}
+              {...props}
+            />
+            <InputGroupAddon addonType="append">
+              <Button
+                className="px-2"
+                data-testid="combobox-caret"
+                disabled={disabled}
+                active={open}
+                onMouseDown={(ev) => {
                 ev.preventDefault();
                 ev.stopPropagation();
                 setOpen(!open);
               }}
-              type="button"
-              tabIndex={-1}
-            >
-              <Icon name={open ? 'caret-up' : 'caret-down'} fixedWidth />
-            </Button>
-          </InputGroupAddon>
-        </InputGroup>
-      </DropdownToggle>
-      <DropdownMenu
-        data-testid="combobox-menu"
-        className="p-0 w-100"
-        modifiers={{
+                type="button"
+                tabIndex={-1}
+              >
+                <Icon name={open ? 'caret-up' : 'caret-down'} fixedWidth />
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </DropdownToggle>
+        <DropdownMenu
+          data-testid="combobox-menu"
+          className="p-0 w-100"
+          modifiers={{
           setMaxHeight: {
             enabled: true,
             fn: (data) => {
@@ -201,14 +245,14 @@ const Combobox: React.FunctionComponent<ComboboxProps> = ({
             },
           },
         }}
-        {...dropdownProps}
-        ref={dropdownMenu}
-      >
-        {visibleOptions
+          {...dropdownProps}
+          ref={dropdownMenu}
+        >
+          {visibleOptions
           .map((option, i) => (
             <DropdownItem
               disabled={option.disabled}
-              key={option.value}
+              key={`${option.value}`}
               active={focusedOptionIndex === i}
               onMouseEnter={(ev) => {
                 ev.preventDefault();
@@ -225,15 +269,16 @@ const Combobox: React.FunctionComponent<ComboboxProps> = ({
               {renderOption(option)}
             </DropdownItem>
           ))}
-        {visibleOptions.length === 0 && (
+          {visibleOptions.length === 0 && (
           <DropdownItem disabled>
             {noResultsLabel}
           </DropdownItem>
         )}
-      </DropdownMenu>
-    </Dropdown>
+        </DropdownMenu>
+      </Dropdown>
+    </>
   );
-};
+}
 
 Combobox.displayName = 'Combobox';
 Combobox.defaultProps = defaultProps;
