d5d823106f6e79a17f7813aa1c5fd59a54c6f796
Darren Eng <darren.eng@appfolio.com>
2021-05-07 14:06:44 -0700

    convert HelpBubble to function component

--- src/components/HelpBubble.js
+++ src/components/HelpBubble.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Icon from './Icon';
 import Popover from './Popover';
-import PopoverHeader from './PopoverHeader';
 import PopoverBody from './PopoverBody';
+import PopoverHeader from './PopoverHeader';
 
 let count = 0;
 
@@ -12,62 +12,51 @@ function getID() {
 }
 
 const style = {
-  cursor: 'pointer'
+  cursor: 'pointer',
 };
 
-class HelpBubble extends React.Component {
-  constructor(props) {
-    super(props);
+function HelpBubble(props) {
+  const idRef = useRef();
+  if (!idRef.current) idRef.current = getID();
+  const id = idRef.current;
 
-    this.id = getID();
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.state = {
-      isOpen: false
-    };
-  }
-
-  toggle = (e) => {
+  const toggle = (e) => {
     e.stopPropagation();
-    this.setState({ isOpen: !this.state.isOpen });
+    setIsOpen(prevIsOpen => !prevIsOpen);
   };
 
   // TODO: remove close and set Popover.toggle to this.toggle once we bump to reactstrap v5
   // Fixes https://github.com/reactstrap/reactstrap/issues/465
-  close = () => {
+  const close = () => {
     setTimeout(() => {
-      this.setState({ isOpen: false });
+      setIsOpen(false);
     });
   };
 
-  render() {
-    const { title, children, className, ...other } = this.props;
+  const { title, children, className, ...other } = props;
 
-    return (
-      <span className={className} style={style}>
-        <Icon
-          name="question-circle"
-          onClick={e => this.toggle(e)}
-          id={this.id}
-          className="text-primary"
-        />
-        <Popover
-          isOpen={this.state.isOpen}
-          toggle={this.close}
-          target={this.id}
-          {...other}
-        >
-          <PopoverHeader>{title}</PopoverHeader>
-          <PopoverBody>{children}</PopoverBody>
-        </Popover>
-      </span>
-    );
-  }
+  return (
+    <span className={className} style={style}>
+      <Icon
+        name="question-circle"
+        onClick={toggle}
+        id={id}
+        className="text-primary"
+      />
+      <Popover isOpen={isOpen} toggle={close} target={id} {...other}>
+        <PopoverHeader>{title}</PopoverHeader>
+        <PopoverBody>{children}</PopoverBody>
+      </Popover>
+    </span>
+  );
 }
 
 HelpBubble.propTypes = {
   title: PropTypes.node.isRequired,
   children: PropTypes.node,
-  className: PropTypes.any
+  className: PropTypes.any,
 };
 
 export default HelpBubble;
