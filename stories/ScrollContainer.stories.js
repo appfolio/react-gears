becfb7bde60138ab8d754b27c703c519165a6c51
Gary Thomas <gary.thomas@appfolio.com>
2021-05-10 13:53:34 -0700

    Update story

--- stories/ScrollContainer.stories.js
+++ stories/ScrollContainer.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { number } from '@storybook/addon-knobs';
+import { number, text } from '@storybook/addon-knobs';
 import ScrollContainer from '../src/components/ScrollContainer';
 
 export default {
@@ -19,6 +19,14 @@ export const MaxHeight = () => (
   <div>
     <ScrollContainer height={number('height', 300)}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag-map_of_the_world.svg/1000px-Flag-map_of_the_world.svg.png" alt="Map" />
+    </ScrollContainer>
+  </div>
+);
+
+export const SaveScrollPosition = () => (
+  <div>
+    <ScrollContainer height={number('height', 500)} scrollPositionKey={text('scrollPositionKey', 'story-example')}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/World_map_2011_CIA_World_Factbook.svg" alt="Map" />
     </ScrollContainer>
   </div>
 );
