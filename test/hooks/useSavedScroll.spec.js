bec981dad0a66df0cb1e1140398423eb1a6bc97f
Phoebe Gao <phoebe.gao@appfolio.com>
2021-05-10 15:53:33 -0700

    add tests

--- test/hooks/useSavedScroll.spec.js
+++ test/hooks/useSavedScroll.spec.js
@@ -1,11 +1,36 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import sinon from 'sinon';
-import * as ReactUse from 'react-use';
-import { renderHook } from '@testing-library/react-hooks';
+import assert from 'assert';
+import { replaceRaf } from 'raf-stub';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useSavedScroll from '../../src/hooks/useSavedScroll';
+
+const mockedPosition = { x: 10, y: 12 };
+const mockSetPosition = jest.fn();
+
+jest.mock('react-use', () => {
+  return {
+    ...(jest.requireActual('react-use')),
+    useSessionStorage: jest.fn(() => [mockedPosition, mockSetPosition]),
+  };
+});
 
 describe('useSavedScroll', () => {
   let div;
+
+  const triggerScroll = (dimension, value) => {
+    if (dimension === 'x') {
+      div.scrollLeft = value;
+    } else if (dimension === 'y') {
+      div.scrollTop = value;
+    }
+
+    div.dispatchEvent(new Event('scroll'));
+  };
+
+  beforeAll(() => {
+    replaceRaf();
+  });
 
   beforeEach(() => {
     div = document.createElement('div');
@@ -15,20 +40,29 @@ describe('useSavedScroll', () => {
 
   afterEach(() => {
     document.body.removeChild(div);
+    requestAnimationFrame.reset();
+    jest.clearAllMocks();
   });
 
   it('should initialize to saved scroll position', () => {
-    const savedPos = { x: 10, y: 12 };
-    sinon.stub(ReactUse, 'useSessionStorage').callsFake(() => [savedPos, () => {}]);
-
     const { result: { current: container } } = renderHook(() => useRef(div));
     const scrollToSpy = sinon.spy(container.current, 'scrollTo');
     renderHook(() => useSavedScroll(container, 'positionKey'));
 
-    sinon.assert.calledWith(scrollToSpy, savedPos);
+    sinon.assert.calledWith(scrollToSpy, mockedPosition.x, mockedPosition.y);
   });
 
   it('should update saved position when scroll position changes', () => {
+    const { result: { current: container } } = renderHook(() => useRef(div));
+    renderHook(() => useSavedScroll(container, 'positionKey'));
 
+    act(() => {
+      triggerScroll('x', 100);
+      triggerScroll('y', 120);
+
+      requestAnimationFrame.step();
+    });
+
+    assert.deepStrictEqual(mockSetPosition.mock.calls[1][0], { x: 100, y: 120 });
   });
 });
