be595e760f6b502bec957f4d6e33df6877c90763
Phoebe Gao <phoebe.gao@appfolio.com>
2021-05-27 20:27:44 -0700

    rename currency input change event

--- stories/CurrencyInput.stories.js
+++ stories/CurrencyInput.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
 import { boolean } from '@storybook/addon-knobs';
 import { CurrencyInput } from '../src';
@@ -6,6 +6,21 @@ import { CurrencyInput } from '../src';
 export default {
   title: 'CurrencyInput',
   component: CurrencyInput,
+};
+
+export const Controlled = () => {
+  const [value, setValue] = useState('100');
+
+  return (
+    <CurrencyInput
+      value={value}
+      onChange={(e) => { setValue(e.target.value); action('onChange')(e); console.log(e); }}
+      allowDecimal={boolean('allowDecimal', true)}
+      allowNegative={boolean('allowNegative', true)}
+      includeThousandsSeparator={boolean('includeThousandsSeparator', true)}
+      padZeros={boolean('padZeros', true)}
+    />
+  );
 };
 
 export const Example = () => (
