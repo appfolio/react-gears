e51359deea7eb85cc3b91efb646d93d81ca480be
Phoebe Gao <phoebe.gao@appfolio.com>
2021-06-02 11:28:36 -0700

    update controlled Popover story
    - implemented toggle prop, so popover would render with the correct position
    - added trigger example

--- stories/Popover.stories.js
+++ stories/Popover.stories.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { boolean, text, select } from '@storybook/addon-knobs';
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import { text, select } from '@storybook/addon-knobs';
 import { Button, Popover, PopoverBody, PopoverHeader, UncontrolledPopover } from '../src';
 
 export default {
@@ -7,28 +8,33 @@ export default {
   component: Popover,
 };
 
-export const LiveExample = () => (
-  <div>
-    <p>
-      I can be placed in context to provide some contextual <strong id="popoverTarget">help</strong>!
-    </p>
-    <Popover
-      isOpen={boolean('isOpen', false)}
-      target="popoverTarget"
-      toggle={() => {}}
-      placement={select('placement', [
+export const LiveExample = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <p>
+        I can be placed in context to provide some contextual <strong id="PopoverExample">help</strong>!
+      </p>
+      <Popover
+        isOpen={open}
+        target="PopoverExample"
+        trigger={select('trigger', ['click', 'hover', 'focus'], 'click')}
+        toggle={(e) => { setOpen(!open); action('toggle')(e); }}
+        placement={select('placement', [
         'top', 'bottom', 'left', 'right'
       ], 'bottom')}
-    >
-      <PopoverBody>
-        <h5>
-          {text('PopoverTitle', 'Hello World')}
-        </h5>
-        {text('PopoverBody', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}
-      </PopoverBody>
-    </Popover>
-  </div>
-);
+      >
+        <PopoverBody>
+          <h5>
+            {text('PopoverTitle', 'Hello World')}
+          </h5>
+          {text('PopoverBody', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}
+        </PopoverBody>
+      </Popover>
+    </div>
+  );
+};
 
 export const Uncontrolled = () => (
   <div>
