194ae13f11e1a2435e9582f06c352a6f0d048a9d
Phoebe Gao <phoebe.gao@appfolio.com>
2021-09-23 16:46:56 -0700

    feat: Add Accordion

---
+++ stories/Accordion.stories.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { boolean } from '@storybook/addon-knobs';
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem, UncontrolledAccordion } from '../src';
+
+export default {
+  title: 'Accordion',
+  component: Accordion,
+};
+
+export const Example = () => {
+  const [open, setOpen] = useState('1');
+  const toggle = (id) => {
+    if (open === id) {
+      setOpen();
+    } else {
+      setOpen(id);
+    }
+  };
+
+  return (
+    <div>
+      <Accordion open={open} toggle={toggle} flush={boolean('flush', false)}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">
+            Accordion Item 1
+          </AccordionHeader>
+          <AccordionBody accordionId="1">
+            <strong>This is the first item's accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader targetId="2">
+            Accordion Item 2
+          </AccordionHeader>
+          <AccordionBody accordionId="2">
+            <strong>This is the second item's accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader targetId="3">
+            Accordion Item 3
+          </AccordionHeader>
+          <AccordionBody accordionId="3">
+            <strong>This is the third item's accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export const Uncontrolled = () => (
+  <div>
+    <UncontrolledAccordion defaultOpen='1' flush={boolean('flush', false)} stayOpen={boolean('stayOpen', false)}>
+      <AccordionItem>
+        <AccordionHeader targetId="1">
+          Accordion Item 1
+        </AccordionHeader>
+        <AccordionBody accordionId="1">
+          <strong>This is the first item's accordion body.</strong>
+          You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        </AccordionBody>
+      </AccordionItem>
+      <AccordionItem>
+        <AccordionHeader targetId="2">
+          Accordion Item 2
+        </AccordionHeader>
+        <AccordionBody accordionId="2">
+          <strong>This is the second item's accordion body.</strong>
+          You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        </AccordionBody>
+      </AccordionItem>
+      <AccordionItem>
+        <AccordionHeader targetId="3">
+          Accordion Item 3
+        </AccordionHeader>
+        <AccordionBody accordionId="3">
+          <strong>This is the third item's accordion body.</strong>
+          You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        </AccordionBody>
+      </AccordionItem>
+    </UncontrolledAccordion>
+  </div>
+);
+
