18cfa057928a622ede438871052f489275c215e6
Phoebe Gao <phoebe.gao@appfolio.com>
2021-09-23 13:46:11 -0700

    feat: Add Offcanvas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import {
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader
+} from 'reactstrap';
 import Badge from './components/Badge';
 import Breadcrumb from './components/Breadcrumb';
 import BreadcrumbItem from './components/BreadcrumbItem';
@@ -232,6 +237,9 @@ export {
   NavbarToggler,
   NavItem,
   NavLink,
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader,
   Pagination,
   PaginationItem,
   PaginationLink,
