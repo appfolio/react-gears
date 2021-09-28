db3797a20e05b47f46fd989b5db159bb2b1ff64c
Phoebe Gao <phoebe.gao@appfolio.com>
2021-09-28 16:36:07 -0700

    Don't add right padding for `city` input in compact mode

--- src/components/AddressInput.js
+++ src/components/AddressInput.js
@@ -144,7 +144,7 @@ class AddressInput extends React.Component {
           />
         </FormLabelGroup>
         <Row className="no-gutters">
-          <Col sm={compact ? undefined : 6} xs={12} className="pr-sm-3">
+          <Col sm={compact ? undefined : 6} xs={12} className={compact ? undefined : 'pr-sm-3'}>
             <FormLabelGroup
               rowClassName={classnames({ 'mb-sm-0': !showCountry })}
               feedback={error.city}
