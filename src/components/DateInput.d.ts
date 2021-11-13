9d364ac9ea679f8c423b77ded0ea09cdff510dee
David <david.acevedo@appfolio.com>
2021-11-12 16:00:14 -0800

    fix: sync DateInput types with PropTypes

--- src/components/DateInput.d.ts
+++ src/components/DateInput.d.ts
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Direction } from 'reactstrap/lib/Dropdown';
 type DateOrString = Date | string;
+type VoidFunction = () => void;
 
 interface DateInputPropTypes extends Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
   'className' | 'size' | 'id' | 'type' | 'onBlur' | 'onChange' | 'onClick' | 'onFocus' | 'onKeyDown' | 'disabled' | 'value' | 'defaultValue'>
 {
   className?: string;
+  dateEnabled?: (value: DateOrString) => boolean;
   dateVisible?: (currentDate: Date) => any[];
   dateFormat?: string;
   defaultValue?: DateOrString;
@@ -16,11 +18,16 @@ interface DateInputPropTypes extends Omit<
   header?: React.ReactNode;
   id?: string;
   keyboard?: boolean;
+  locale?: any;
   onBlur?: (ev: React.FocusEvent<HTMLInputElement>) => void;
   onChange?: (value: DateOrString, isDate: boolean) => void;
   onClose?: (value: DateOrString, isDate: boolean) => void;
   parse?: (value: string, dateFormat: string) => DateOrString;
+  positionFixed?: boolean;
+  renderFooter?: (today?: VoidFunction, clear?: VoidFunction) => React.Component;
+  renderHeader?: (prevMonth: VoidFunction, nextMonth: VoidFunction, prevYear: VoidFunction, nextYear: VoidFunction) => React.Component;
   showOnFocus?: boolean;
+  state?: any;
   value?: DateOrString;
 }
 
