04546ddd73d482ad847d2af511547fa38210a2bc
David <david.acevedo@appfolio.com>
2021-12-10 18:06:51 -0800

    feat: add form-group class into FormGroup
    We still use form-group internally for a lot of tests, so this class is required

--- src/components/FormGroup.tsx
+++ src/components/FormGroup.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
 import { FormGroup } from 'reactstrap';
+import classnames from 'classnames';
 
-export default FormGroup;
+export default (props: any) => {
+  let className = 'form-group';
+  if (props.className) {
+    // Need form-group for property tests compatibility
+    className = classnames(props.className, 'form-group');
+  }
+
+  return (
+    <FormGroup {...props} className={className} />
+  );
+};
