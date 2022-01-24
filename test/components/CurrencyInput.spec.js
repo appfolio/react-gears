7db5a5bf1fb326195a78433cbc020753897235fd
Steven Than <thuyen.than@appfolio.com>
2022-01-24 12:44:09 -0800

    chore(CurrencyInput): covert test to pure jest

--- test/components/CurrencyInput.spec.js
+++ test/components/CurrencyInput.spec.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import assert from 'assert';
 import { mount } from 'enzyme';
-import sinon from 'sinon';
 
 import { CurrencyInput } from '../../src';
 
 describe('<CurrencyInput />', () => {
-  const callback = sinon.spy();
+  const callback = jest.fn();
 
   it('should default to blank', () => {
     const component = mount(<CurrencyInput onChange={callback} />);
     const input = component.find('input');
 
-    assert.equal(input.getDOMNode().value, '');
+    expect(input.getDOMNode().value).toBe('');
   });
 
   it('should format value prop', () => {
     const component = mount(<CurrencyInput onChange={callback} value={1234.56} />);
     const input = component.find('input');
 
-    assert.equal(input.getDOMNode().value, '1,234.56');
+    expect(input.getDOMNode().value).toBe('1,234.56');
   });
 
   it('should not reset uncontrolled input value on re-renders', () => {
@@ -37,13 +35,13 @@ describe('<CurrencyInput />', () => {
     it('should default to true', () => {
       const component = mount(<CurrencyInput onChange={callback} value="123,456,789.99999" />);
       const input = component.find('input');
-      assert.equal(input.getDOMNode().value, '123,456,789.99');
+      expect(input.getDOMNode().value).toBe('123,456,789.99');
     });
 
     it('can be disabled', () => {
       const component = mount(<CurrencyInput value="123,456" includeThousandsSeparator={false} />);
       const input = component.find('input');
-      assert.equal(input.getDOMNode().value, '123456.00');
+      expect(input.getDOMNode().value).toBe('123456.00');
     });
   });
 
@@ -51,13 +49,13 @@ describe('<CurrencyInput />', () => {
     it('should be disabled by default', () => {
       const component = mount(<CurrencyInput value="-123" />);
       const input = component.find('input');
-      assert.equal(input.getDOMNode().value, '123.00');
+      expect(input.getDOMNode().value).toBe('123.00');
     });
 
     it('can be enabled', () => {
       const component = mount(<CurrencyInput value="-123" allowNegative />);
       const input = component.find('input');
-      assert.equal(input.getDOMNode().value, '-123.00');
+      expect(input.getDOMNode().value).toBe('-123.00');
     });
   });
 
@@ -65,61 +63,61 @@ describe('<CurrencyInput />', () => {
     it('should be enabled by default', () => {
       const component = mount(<CurrencyInput value="123.45" />);
       const input = component.find('input');
-      assert.equal(input.getDOMNode().value, '123.45');
+      expect(input.getDOMNode().value).toBe('123.45');
     });
 
     it('can be disabled', () => {
       const component = mount(<CurrencyInput value="123.45" allowDecimal={false} />);
       const input = component.find('input');
-      assert.equal(input.getDOMNode().value, '12,345');
+      expect(input.getDOMNode().value).toBe('12,345');
     });
 
     it('should truncate more than 2 decimals', () => {
       const component = mount(<CurrencyInput value={1234.56789} />);
       const input = component.find('input');
 
-      assert.equal(input.getDOMNode().value, '1,234.56');
+      expect(input.getDOMNode().value).toBe('1,234.56');
     });
 
     it('should ignore additional decimal points', () => {
       const component = mount(<CurrencyInput value="1234.56." />);
       const input = component.find('input');
 
-      assert.strictEqual(input.getDOMNode().value, '1,234.56');
+      expect(input.getDOMNode().value).toBe('1,234.56');
     });
 
     it('should prefix decimal only currency with 0', () => {
       const component = mount(<CurrencyInput onChange={callback} value="." />);
       const input = component.find('input');
 
-      assert.strictEqual(input.getDOMNode().value, '0.00');
+      expect(input.getDOMNode().value).toBe('0.00');
     });
 
     it('should zero pad 1 decimal', () => {
       const component = mount(<CurrencyInput onChange={callback} value={1234.5} />);
       const input = component.find('input');
 
-      assert.equal(input.getDOMNode().value, '1,234.50');
+      expect(input.getDOMNode().value).toBe('1,234.50');
     });
 
-    it.skip('should zero pad no decimals', () => {
+    it('should zero pad no decimals', () => {
       const component = mount(<CurrencyInput onChange={callback} value={1234} />);
       const input = component.find('input');
 
-      assert.equal(input.getDOMNode().value, '1,234.00');
+      expect(input.getDOMNode().value).toBe('1,234.00');
     });
   });
 
   it('should not format invalid values', () => {
     const component = mount(<CurrencyInput onChange={callback} value="Veni, Vedi, Vici" />);
     const input = component.find('input');
-    assert.equal(input.getDOMNode().value, '');
+    expect(input.getDOMNode().value).toBe('');
   });
 
   it('should pass inputProps', () => {
     const component = mount(<CurrencyInput inputProps={{ className: 'yowza', ariaLabel: 'yowza!' }} />);
     const input = component.find('input.yowza');
-    assert(input.exists());
-    assert.equal(input.props().ariaLabel, 'yowza!');
+    expect(input.exists()).toBe(true);
+    expect(input.props().ariaLabel).toBe('yowza!');
   });
 });
