45101c544ca737e5fa1446ac9398b50c1b87e224
David <david.acevedo@appfolio.com>
2022-03-30 19:28:33 -0700

    test: add @testing-library/jest-dom dom helper methods
    https://github.com/testing-library/jest-dom#the-problem

--- src/tooling/helpers.ts
+++ src/tooling/helpers.ts
@@ -1,6 +1,7 @@
 import 'regenerator-runtime';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom'
 
 configure({ adapter: new Adapter() });
 
