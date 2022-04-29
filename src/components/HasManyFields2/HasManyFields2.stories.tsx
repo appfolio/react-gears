9318d8b810fa982977d24732da1f00d3d17b5105
Jupp Mueller <jupp.mueller@appfolio.com>
2022-04-29 13:31:15 -0700

    Add mobx integration

--- src/components/HasManyFields2/HasManyFields2.stories.tsx
+++ src/components/HasManyFields2/HasManyFields2.stories.tsx
@@ -1,63 +1,108 @@
 import { action } from '@storybook/addon-actions';
 import { boolean, number, text } from '@storybook/addon-knobs';
+import { action as mobxAction, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import AddressInput from '../Address/AddressInput';
 import { HasManyFields2, HasManyFields2Row } from './HasManyFields2';
-
-
 
 export default {
   title: 'HasManyFields2',
   component: HasManyFields2,
 };
 
-export const LiveExample = () => (
-  <HasManyFields2
-    label={text("label", "Add an Address")}
-    disabled={boolean('disabled', false)}
-    onAdd={action('hasManyFields onAdd')}
-    maximumRows={number('maximumRows', 5)}
-    minimumRows={number('minimumRows', 0)}
-    reorderable={boolean('reorderable', true)}
-  >
-    <HasManyFields2Row rowId="row1" disabled={false}>
-      <AddressInput
-        value={{
-          address1: '70 Castilian Dr.',
-          address2: '',
-          city: 'Goleta',
-          state: 'CA',
-          postal: '93117',
-          countryCode: 'US',
-        }}
-      />
-    </HasManyFields2Row>
-    <HasManyFields2Row rowId="row2">
-      <AddressInput
-        value={{
-          address1: '90 Castilian Dr.',
-          address2: '',
-          city: 'Goleta',
-          state: 'CA',
-          postal: '93117',
-          countryCode: 'US',
-        }}
-      />
-    </HasManyFields2Row>
-  </HasManyFields2>
-);
+type Address = {
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  postal: string;
+  countryCode: string;
+  rowId: string;
+};
+class AddressStore {
+  addresses: Address[] = [
+    {
+      address1: '90 Castilian Dr.',
+      address2: '',
+      city: 'Goleta',
+      state: 'CA',
+      postal: '93117',
+      countryCode: 'US',
+      rowId: crypto.randomUUID(),
+    },
+    {
+      address1: '70 Castilian Dr.',
+      address2: '',
+      city: 'Goleta',
+      state: 'CA',
+      postal: '93117',
+      countryCode: 'US',
+      rowId: crypto.randomUUID(),
+    },
+  ];
 
-// export const RowWrapper = () => (
-//   <HasManyFieldsRow
-//     onDelete={action('onDelete')}
-//     disabled={boolean('disabled', false)}
-//     disabledReason={text('disabledReason')}
-//     disabledReasonPlacement={select('placement', ['top', 'left', 'bottom', 'right'], 'top')}
-//   >
-//     <Input defaultValue="I can put an input (or whatever else) inside a HasManyFieldsRow" />
-//   </HasManyFieldsRow>
-// );
+  order: string[] = this.addresses.map(address => address.rowId);
 
-// export const AddItemButton = () => (
+  constructor() {
+    makeObservable(this, {
+      addresses: observable,
+      order: observable,
+      addAddress: mobxAction,
+      orderChanged: mobxAction,
+    });
+    
+  }
 
-// );
+  addAddress() {
+    const defaultAddress = {
+      address1: '90 Castilian Dr.',
+      address2: '',
+      city: 'Goleta',
+      state: 'CA',
+      postal: '93117',
+      countryCode: 'US',
+      rowId: crypto.randomUUID(),
+    };
+    this.addresses.push(defaultAddress);
+    this.order.push(defaultAddress.rowId);
+    console.log("addAddress", this.addresses);
+  }
+
+  orderChanged(order: string[]) {
+    console.log("order changed", order);
+    this.order = order;
+  }
+}
+const addressStore = new AddressStore();
+
+const HasManyAddresses: React.FC<{store: AddressStore}> = observer(({store}) => (
+    <HasManyFields2
+      label={text('label', 'Add an Address')}
+      disabled={boolean('disabled', false)}
+      onAdd={() => {
+        store.addAddress();
+        action('hasManyFields onAdd');
+      }}
+      onOrderChanged={(order) => {
+        store.orderChanged(order);
+        action('order changed');
+      }}
+      maximumRows={number('maximumRows', 5)}
+      minimumRows={number('minimumRows', 0)}
+      reorderable={boolean('reorderable', true)}
+    >
+      {store.addresses.map((address) => (
+        <HasManyFields2Row
+          rowId={address.rowId}
+          disabled={false}
+          disabledReason={undefined}
+          disabledReasonPlacement={undefined}
+        >
+          <AddressInput value={address} />
+        </HasManyFields2Row>
+      ))}
+    </HasManyFields2>
+  ));
+
+  export const LiveExample = () => (<HasManyAddresses store={addressStore} />);
