6789250b16126b2c99d142aa152e2ab17259bfcd
Loc Nguyen <loc.nguyen@appfolio.com>
2022-06-10 11:12:37 -0700

    fixup! refactor: update Spinner to typescript

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,14 +1,14 @@
-import React, { } from 'react';
+import React from 'react';
 import { Spinner, SpinnerProps } from 'reactstrap';
 import ApmSpinner from './ApmSpinner';
 
-const SpinnerWrapper = ({type, ...props }: SpinnerProps) =>
+const SpinnerWrapper = ({ type, ...props }: SpinnerProps) =>
   type === 'spin' ? <ApmSpinner {...props} /> : <Spinner type={type} {...props} />;
 
 SpinnerWrapper.defaultProps = {
   // TODO: update, Spinner doesn't contain the defaultProps types so we cast as any
   ...(Spinner as any).defaultProps,
-  type: 'spin'
+  type: 'spin',
 };
 
 SpinnerWrapper.displayName = 'Spinner';
