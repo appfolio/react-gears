39311404e0fe7029950dc417baff880f3a2e88ce
Loc Nguyen <loc.nguyen@appfolio.com>
2022-06-24 16:05:51 -0700

    Chore: typify component

---
+++ src/components/Input/FileInput.tsx
@@ -0,0 +1,26 @@
+
+import React, { ChangeEvent } from 'react';
+import Input from './Input';
+
+type FileInputProps = {
+  onChange: (files: FileList | null) => void;
+  type: 'file';
+}
+
+class FileInput extends React.Component<FileInputProps> {
+  handleChange = (changeEvent: ChangeEvent) => {
+    const files = (changeEvent.target as HTMLInputElement).files;
+    if (this.props.onChange) {
+      this.props.onChange(files);
+    }
+  };
+
+  render() {
+    /* eslint-disable-next-line  @typescript-eslint/no-unused-vars -- Let's figure out a better way to omit props for this scenario */
+    const { type, onChange, ...props } = this.props;
+
+    return <Input type="file" onChange={this.handleChange} {...props} />;
+  }
+}
+
+export default FileInput;
