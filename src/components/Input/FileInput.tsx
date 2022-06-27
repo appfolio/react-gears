cd67fac4bbc35d60e9882b227495b5364949b9e4
Loc Nguyen <loc.nguyen@appfolio.com>
2022-06-27 11:54:11 -0700

    refactor: convert to functional component

--- src/components/Input/FileInput.tsx
+++ src/components/Input/FileInput.tsx
@@ -1,25 +1,19 @@
 import React, { ChangeEvent } from 'react';
 import Input from './Input';
 
-type FileInputProps = {
+interface FileInputProps extends Omit<React.ComponentProps<typeof Input>, 'type'> {
   onChange: (files: FileList | null) => void;
-  type: 'file';
-};
+}
 
-class FileInput extends React.Component<FileInputProps> {
-  handleChange = (changeEvent: ChangeEvent) => {
-    const files = (changeEvent.target as HTMLInputElement).files;
-    if (this.props.onChange) {
-      this.props.onChange(files);
-    }
+const FileInput = (props: FileInputProps) => {
+  const { onChange } = props;
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    onChange?.(files);
   };
 
-  render() {
-    /* eslint-disable-next-line  @typescript-eslint/no-unused-vars -- Let's figure out a better way to omit props for this scenario */
-    const { type, onChange, ...props } = this.props;
-
-    return <Input type="file" onChange={this.handleChange} {...props} />;
-  }
-}
+  return <Input type="file" onChange={handleChange} />;
+};
 
 export default FileInput;
