920ba7a9f8b7d3891b1760012a54dad423e9b7b7
JeremyRH <holcomb227@gmail.com>
2022-08-26 14:42:03 -0700

    chore: run yarn format

--- src/components/Form/FormRow.tsx
+++ src/components/Form/FormRow.tsx
@@ -78,7 +78,9 @@ function FormRow<T extends TypeProp>({
   width,
   ...props
 }: FormRowProps<T>) {
-  const InputComponent = (typeof type === 'string' ? getInputByType(type as InputTypes) : type) as any;
+  const InputComponent = (
+    typeof type === 'string' ? getInputByType(type as InputTypes) : type
+  ) as any;
   const [baseFeedback, childFeedback] = parseFeedback(feedback);
   const shouldPassChildren = type === 'checkbox' || type === 'radio' || type === 'select';
   const validityThings = shouldPassChildren ? { valid: !!validFeedback, invalid: !!feedback } : {};
