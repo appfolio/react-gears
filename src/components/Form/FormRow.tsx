9a9cf5b41bc74ca52f3445ac7b77d71af8c0dbeb
Fabio Aiello <fabio.aiello@appfolio.com>
2023-03-27 16:27:50 -0700

    fix: run prettier

--- src/components/Form/FormRow.tsx
+++ src/components/Form/FormRow.tsx
@@ -111,7 +111,15 @@ function FormRow<T extends TypeProp>({
       validFeedback={validFeedback}
       width={width}
     >
-      <InputComponent id={id} size={size} aria-required={required} className='test' {...validProps} {...props} {...childFeedback}>
+      <InputComponent
+        id={id}
+        size={size}
+        aria-required={required}
+        className="test"
+        {...validProps}
+        {...props}
+        {...childFeedback}
+      >
         {memoChildren}
       </InputComponent>
     </FormLabelGroup>
