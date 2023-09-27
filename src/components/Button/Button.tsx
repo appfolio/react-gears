3d55ece31b8b5c782a1b0855fe8676778a3bf1f5
Fabio Aiello <fabio.aiello@appfolio.com>
2023-09-26 17:09:23 -0700

    feat: add aria attributes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,17 @@
-import { Button } from 'reactstrap';
+import React from 'react';
+import { Button as RsButton } from 'reactstrap';
+
+const Button = ({ disabled = false, block = false, outline = false, ...props }) => (
+  <RsButton
+    {...props}
+    disabled={disabled}
+    block={block}
+    outline={outline}
+    role="button"
+    aria-disabled={disabled}
+    aria-labelledby={typeof props.children === 'string' ? props.children : undefined}
+  />
+);
 
 export type { ButtonProps } from 'reactstrap';
 
