36e80bf6ee0eb2615676d53c07c121c9e797be76
Fabio Aiello <fabio.aiello@appfolio.com>
2023-12-21 12:19:50 -0800

    fix: further react-gears update

--- src/components/Input/MaskedInput.tsx
+++ src/components/Input/MaskedInput.tsx
@@ -1,14 +1,8 @@
 import React, { FC } from 'react';
-import MaskedInputBase, { MaskedInputProps as RTMaskedInputProps } from 'react-text-mask';
+import MaskedInputBase, { MaskedInputProps } from 'react-text-mask';
 
-interface MaskedInputProps extends RTMaskedInputProps {
-  'data-testid'?: string;
-}
+const MaskedInput: FC<MaskedInputProps> = ({ guide = false, ...props }) => (
+  <MaskedInputBase className="form-control" guide={guide} {...props} />
+);
 
-const MaskedInput: FC<MaskedInputProps> = ({ guide = false, ...props }) => {
-  if (props && !props['data-testid']) {
-    props['data-testid'] = 'react-gears-maskedinput';
-  }
-  return <MaskedInputBase className="form-control" guide={guide} {...props} />;
-};
 export default MaskedInput;
