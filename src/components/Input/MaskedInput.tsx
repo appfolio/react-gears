13c99fef15883a87b4ccb0464b4b2dc253831bdb
fabio-aiello-appfolio <80420263+fabio-aiello-appfolio@users.noreply.github.com>
2024-01-02 15:22:36 -0800

    Revert "fix: reword import and make prop optional"
    This reverts commit ee3f261dc5ccfdb0c961cecce2071555aa927114.

--- src/components/Input/MaskedInput.tsx
+++ src/components/Input/MaskedInput.tsx
@@ -1,14 +1,20 @@
 import React, { FC } from 'react';
 import MaskedInputBase, { MaskedInputProps as RTMaskedInputProps } from 'react-text-mask';
 
-interface MaskedInputProps extends RTMaskedInputProps {
-  'data-testid'?: string;
+export interface MaskedInputProps extends RTMaskedInputProps {
+  'data-testid': string;
 }
 
-const MaskedInput: FC<MaskedInputProps> = ({ guide = false, ...props }) => {
-  if (props && !props['data-testid']) {
-    props['data-testid'] = 'react-gears-maskedinput';
-  }
-  return <MaskedInputBase className="form-control" guide={guide} {...props} />;
+const MaskedInput: FC<MaskedInputProps> = ({
+  guide = false,
+  'data-testid': dataTestId,
+  ...props
+}) => {
+  dataTestId = dataTestId || 'react-gears-masked-input';
+
+  return (
+    <MaskedInputBase className="form-control" guide={guide} data-testid={dataTestId} {...props} />
+  );
 };
+
 export default MaskedInput;
