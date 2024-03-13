52a6b48e7ea655fd67a93afb1c245921e079a0a4
Fabio Aiello <fabio.aiello@appfolio.com>
2024-03-13 16:56:01 -0700

    fix: correct tests

--- src/components/SummaryBox/SummaryBoxItem.spec.js
+++ src/components/SummaryBox/SummaryBoxItem.spec.js
@@ -7,14 +7,14 @@ describe('<SummaryBoxItem />', () => {
   it('should render correctly', () => {
     const component = mount(<SummaryBoxItem label="Hello" value="World" />);
     assert(component);
-    assert(component.find('.h3').text(), 'Hello');
+    assert(component.find('.display-4').text(), 'Hello');
     assert(component.find('span').text(), 'World');
   });
 
   it('should show the default label and value if none specified', () => {
     const component = mount(<SummaryBoxItem />);
     assert(component);
-    assert(component.find('.h3').text(), '--');
+    assert(component.find('.display-4').text(), '--');
     assert(component.find('span').text(), '--');
   });
 
