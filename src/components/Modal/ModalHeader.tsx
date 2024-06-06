ef88a5e19ae848610d45c9b8845097f8518f4c1b
Brian Holland <56670523+Brian-Holland@users.noreply.github.com>
2024-06-06 18:11:54 -0500

    refactor: add displayName to ModalHeader

--- src/components/Modal/ModalHeader.tsx
+++ src/components/Modal/ModalHeader.tsx
@@ -3,4 +3,6 @@ import { ModalHeader as RsModalHeader } from 'reactstrap';
 
 const ModalHeader = ({ ...props }) => <RsModalHeader tag="h2" {...props} />;
 
+ModalHeader.displayName = 'ReactGearsModalHeader';
+
 export default ModalHeader;
