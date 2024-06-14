66d5d833d194c293f96534b3092bf00d99b5563e
Brian Holland <56670523+Brian-Holland@users.noreply.github.com>
2024-06-14 17:48:10 -0500

    remove displayname to fix tests

--- src/components/Modal/ModalHeader.tsx
+++ src/components/Modal/ModalHeader.tsx
@@ -3,6 +3,4 @@ import { ModalHeader as RsModalHeader } from 'reactstrap';
 
 const ModalHeader = ({ ...props }) => <RsModalHeader tag="h2" {...props} />;
 
-ModalHeader.displayName = 'ReactGearsModalHeader';
-
 export default ModalHeader;
