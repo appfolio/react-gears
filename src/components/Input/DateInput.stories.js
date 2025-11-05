06dcca8be4663ef8eea763a743924ff3d8248094
Fabio Aiello <fabio.aiello@appfolio.com>
2025-11-05 10:36:46 -0800

    fix: reinstall

--- src/components/Input/DateInput.stories.js
+++ src/components/Input/DateInput.stories.js
@@ -176,7 +176,7 @@ export const WithContainer = (args) => (
       style={{
         position: 'relative',
         overflow: 'hidden',
-        height: '315px',
+        height: '310px',
         border: '1px solid #ccc',
         padding: '10px',
       }}
